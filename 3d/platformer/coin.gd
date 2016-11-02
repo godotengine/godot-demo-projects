ed9494f2516a8261d52c5ee179db97bdca8c0a36
William Tumeo <tumeowilliam@gmail.com>
2016-11-02 17:08:20 -0200

    free coin in 3D platformer
    (cherry picked from commit 76bfb10deb9a9eaeb206700e60e716465294d246)

--- 3d/platformer/coin.gd
+++ 3d/platformer/coin.gd
@@ -9,3 +9,9 @@ func _on_coin_body_enter(body):
 	if (not taken and body extends preload("res://player.gd")):
 		get_node("anim").play("take")
 		taken = true
+
+
+func _on_anim_finished():
+	if get_node("anim").get_current_animation() == "take":
+		queue_free()
+		
