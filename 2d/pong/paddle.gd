13eab7d1aa78ea1b498ca9f30323d18c76a0d42e
Juan Linietsky <reduzio@gmail.com>
2017-07-01 07:29:03 -0300

    Added visual script pong

--- 2d/pong/paddle.gd
+++ 2d/pong/paddle.gd
@@ -10,7 +10,7 @@ func _process(delta):
 	# move up and down based on input
 	if (Input.is_action_pressed(which+"_move_up") and position.y > 0):
 		position.y -= MOVE_SPEED * delta
-	if (Input.is_action_pressed(which+"_move_down") and position.y < 640):
+	if (Input.is_action_pressed(which+"_move_down") and position.y < get_viewport_rect().size.y):
 		position.y += MOVE_SPEED * delta
 		
 
