b6390ebc4dee9d027e03f29624c129825febc6f8
eon-s <eon-s@users.noreply.github.com>
2017-08-28 20:14:53 -0300

    Added platformer demo with kinematic character from 2.2
    Has some fixes on the input handling (replacing just_pressed), fix motion enabled, corrections on enemy scene and tilemap.

---
+++ 2d/platformer_kcc/bullet.gd
@@ -0,0 +1,9 @@
+
+extends RigidBody2D
+
+func _on_bullet_body_enter( body ):
+	if (body.has_method("hit_by_bullet")):
+		body.call("hit_by_bullet")
+	
+func _on_Timer_timeout():
+	get_node("anim").play("shutdown")
