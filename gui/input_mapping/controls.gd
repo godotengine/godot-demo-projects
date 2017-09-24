71e832089dff13fae0e14d52adcb5de373719880
Rémi Verschelde <rverschelde@gmail.com>
2017-09-24 16:58:38 +0200

    Input mapping: Add code to load/save config

--- gui/input_mapping/controls.gd
+++ gui/input_mapping/controls.gd
@@ -1,4 +1,3 @@
-
 extends Control
 
 # Note for the reader:
@@ -12,11 +11,55 @@ extends Control
 # action and the node, e.g.:
 # button.connect("pressed", self, "wait_for_input", [ button, action ])
 
+# Constants
+const INPUT_ACTIONS = [ "move_up", "move_down", "move_left", "move_right", "jump" ]
+const CONFIG_FILE = "user://input.cfg"
+
 # Member variables
-var player_actions = [ "move_up", "move_down", "move_left", "move_right", "jump" ]
 var action # To register the action the UI is currently handling
 var button # Button node corresponding to the above action
 
+
+# Load/save input mapping to a config file
+# Changes done while testing the demo will be persistent, saved to CONFIG_FILE
+
+func load_config():
+	var config = ConfigFile.new()
+	var err = config.load(CONFIG_FILE)
+	if err: # Assuming that file is missing, generate default config
+		for action_name in INPUT_ACTIONS:
+			var action_list = InputMap.get_action_list(action_name)
+			# There could be multiple actions in the list, but we save the first one by default
+			var scancode = OS.get_scancode_string(action_list[0].scancode)
+			config.set_value("input", action_name, scancode)
+		config.save(CONFIG_FILE)
+	else: # ConfigFile was properly loaded, initialize InputMap
+		for action_name in config.get_section_keys("input"):
+			# Get the key scancode corresponding to the saved human-readable string
+			var scancode = OS.find_scancode_from_string(config.get_value("input", action_name))
+			# Create a new event object based on the saved scancode
+			var event = InputEvent()
+			event.type = InputEvent.KEY
+			event.scancode = scancode
+			# Replace old action (key) events by the new one
+			for old_event in InputMap.get_action_list(action_name):
+				if old_event.type == InputEvent.KEY:
+					InputMap.action_erase_event(action_name, old_event)
+			InputMap.action_add_event(action_name, event)
+
+
+func save_to_config(section, key, value):
+	"""Helper function to redefine a parameter in the settings file"""
+	var config = ConfigFile.new()
+	var err = config.load(CONFIG_FILE)
+	if err:
+		print("Error code when loading config file: ", err)
+	else:
+		config.set_value(section, key, value)
+		config.save(CONFIG_FILE)
+
+
+# Input management
 
 func wait_for_input(action_bind):
 	action = action_bind
@@ -28,28 +71,31 @@ func wait_for_input(action_bind):
 
 func _input(event):
 	# Handle the first pressed key
-	if (event.type == InputEvent.KEY):
+	if event.type == InputEvent.KEY:
 		# Register the event as handled and stop polling
 		get_tree().set_input_as_handled()
 		set_process_input(false)
 		# Reinitialise the contextual help label
 		get_node("contextual_help").set_text("Click a key binding to reassign it, or press the Cancel action.")
-		if (not event.is_action("ui_cancel")):
+		if not event.is_action("ui_cancel"):
 			# Display the string corresponding to the pressed key
-			button.set_text(OS.get_scancode_string(event.scancode))
+			var scancode = OS.get_scancode_string(event.scancode)
+			button.set_text(scancode)
 			# Start by removing previously key binding(s)
 			for old_event in InputMap.get_action_list(action):
 				InputMap.action_erase_event(action, old_event)
 			# Add the new key binding
 			InputMap.action_add_event(action, event)
+			save_to_config("input", action, scancode)
 
 
 func _ready():
+	# Load config if existing, if not it will be generated with default values
+	load_config()
 	# Initialise each button with the default key binding from InputMap
-	var input_event
-	for action in player_actions:
+	for action in INPUT_ACTIONS:
 		# We assume that the key binding that we want is the first one (0), if there are several
-		input_event = InputMap.get_action_list(action)[0]
+		var input_event = InputMap.get_action_list(action)[0]
 		# See note at the beginning of the script
 		var button = get_node("bindings").get_node(action).get_node("Button")
 		button.set_text(OS.get_scancode_string(input_event.scancode))
