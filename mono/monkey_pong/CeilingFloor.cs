3449af31437b2ed2a1366f87a9597cb26dc7304e
Jonathan <j.sweet90@hotmail.com>
2018-01-04 01:09:34 -0600

    fixed ball direction change
    Got rid of unnecessary variable and changed it to properly change to the opposite y direction when bouncing off the floor or ceiling.

--- mono/monkey_pong/CeilingFloor.cs
+++ mono/monkey_pong/CeilingFloor.cs
@@ -3,14 +3,11 @@ using System;
 
 public class CeilingFloor : Area2D
 {
-    [Export]
-    private int yDirection = 1;
-
     public void OnAreaEntered(Area2D area)
     {
         if (area is Ball ball)
         {
-            ball.direction += new Vector2(0, yDirection);
+            ball.direction += new Vector2(ball.direction.x, -ball.direction.y);
         }
     }
 }
