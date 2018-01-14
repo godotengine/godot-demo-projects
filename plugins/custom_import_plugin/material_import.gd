0bf5a9935dac9bb1b47be67fe73b74846a71176f
George Marques <george@gmarqu.es>
2018-01-14 18:29:49 -0200

    Add a custom import plugin demo
    This is a direct port from the Godot 2.1 version of the demo.

---
+++ plugins/custom_import_plugin/material_import.gd
@@ -0,0 +1,13 @@
+tool
+extends EditorPlugin
+
+var import_plugin
+
+func _enter_tree():
+	import_plugin = preload("import_plugin.gd").new()
+
+	add_import_plugin(import_plugin)
+
+func _exit_tree():
+	remove_import_plugin(import_plugin)
+	import_plugin = null
