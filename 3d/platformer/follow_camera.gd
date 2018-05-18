86bf5060b349dc0d7e485c9d8b8a245f68eeac30
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-05-18 22:52:52 +0200

    Use an higher FOV value in demos
    A vertical FOV of 74 degrees is roughly equivalent to a 90 degree
    horizontal FOV on a 4:3 display (~106.26 degrees on 16:9), which
    is the typical default FOV used in PC games.

--- 3d/platformer/follow_camera.gd
+++ 3d/platformer/follow_camera.gd
@@ -4,7 +4,7 @@ extends Camera
 # Member variables
 var collision_exception = []
 export var min_distance = 0.5
-export var max_distance = 4.0
+export var max_distance = 3.5
 export var angle_v_adjust = 0.0
 export var autoturn_ray_aperture = 25
 export var autoturn_speed = 50
@@ -15,33 +15,33 @@ func _physics_process(dt):
 	var target = get_parent().get_global_transform().origin
 	var pos = get_global_transform().origin
 	var up = Vector3(0, 1, 0)
-	
+
 	var delta = pos - target
-	
+
 	# Regular delta follow
-	
+
 	# Check ranges
 	if  delta.length() < min_distance:
 		delta = delta.normalized() * min_distance
 	elif  delta.length() > max_distance:
 		delta = delta.normalized() * max_distance
-	
+
 	# Check upper and lower height
 	if delta.y > max_height:
 		delta.y = max_height
 	if delta.y < min_height:
 		delta.y = min_height
-		
+
 	# Check autoturn
 	var ds = PhysicsServer.space_get_direct_state(get_world().get_space())
-	
+
 	var col_left = ds.intersect_ray(target, target + Basis(up, deg2rad(autoturn_ray_aperture)).xform(delta), collision_exception)
 	var col = ds.intersect_ray(target, target + delta, collision_exception)
 	var col_right = ds.intersect_ray(target, target + Basis(up, deg2rad(-autoturn_ray_aperture)).xform(delta), collision_exception)
-	
+
 	if !col.empty():
 		# If main ray was occluded, get camera closer, this is the worst case scenario
-		delta = col.position - target	
+		delta = col.position - target
 	elif !col_left.empty() and col_right.empty():
 		# If only left ray is occluded, turn the camera around to the right
 		delta = Basis(up, deg2rad(-dt * autoturn_speed)).xform(delta)
@@ -51,15 +51,15 @@ func _physics_process(dt):
 	else:
 		# Do nothing otherwise, left and right are occluded but center is not, so do not autoturn
 		pass
-	
+
 	# Apply lookat
 	if delta == Vector3():
 		delta = (pos - target).normalized() * 0.0001
 
 	pos = target + delta
-	
+
 	look_at_from_position(pos, target, up)
-	
+
 	# Turn a little up or down
 	var t = get_transform()
 	t.basis = Basis(t.basis[0], deg2rad(angle_v_adjust)) * t.basis
