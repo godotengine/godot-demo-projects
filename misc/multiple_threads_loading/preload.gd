8a3556fb9dab36dc8828a5c96f9c247eca60a959
Vitaliy <vita-zhelt@yandex.ru>
2018-09-16 12:46:42 +0300

    Add files via upload

---
+++ misc/multiple_threads_loading/preload.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var queue
+
+func _ready():
+	queue = preload("res://resource-queue.gd").new()
+	# Call after you instance the class to start the thread.
+	queue.start()
+
+func _process(delta):
+	# Returns true if a resource is done loading and ready to be retrieved
+	if queue.is_ready("res://main.tscn"):
+		set_process(false)
+		# Returns the fully loaded resource
+		var next_scene = queue.get_resource("res://main.tscn").instance()
+		get_node("/root").add_child(next_scene)
+		get_node(".").queue_free()
+		get_node("/root").remove_child(get_node("."))
+	else:
+		# Get the progress of a resource
+		var progress = round(queue.get_progress("res://main.tscn")*100)
+		get_node("ProgressBar").set_value(progress)
+
+func _on_Button_button_up():
+	set_process(true)
+	# Queue a resource
+	queue.queue_resource("res://main.tscn", true)
