106b893e31ba9f4546dd55b8f7915f7e15de4c12
Raymond Hulha <java4life@gmail.com>
2018-12-02 14:35:27 +0100

    I improved the custom_dock plugin after some personal hiccups trying to get a dock button to work. I think supplying a custom_dock plugin demo where one of the buttons actually does something is more informative and helpful for beginners like me.
    Changes:
    Added a script for the custom_dock button.
    Implemented the button pressed event.
    The button press now executes: print("Plugin Button Pressed!")

---
+++ plugins/custom_dock/button.gd
@@ -0,0 +1,20 @@
+tool
+extends Button
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Button_pressed():
+	print("Plugin Button Pressed!")
