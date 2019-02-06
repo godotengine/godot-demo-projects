9adc81ce7f347da34c794b328119765f81ef7776
Wagner Ferreira <greyfox_esb@hotmail.com>
2019-02-06 14:13:52 -0200

    Added extra rewriting in the code to improve mouse drag
    * Made the whole block a separated scene to demonstrate it can work across multiple instances.
    * Dropped the Area.input_event and did everything manually with raycasting.
    * Made quad_mesh_size automatically pick the quad size, avoiding the extra setup.
    * Changed from PlaneMesh to QuadMesh. Now everyting can start with 0 rotation.
    * The function will keep handling input when the mouse is outside of the area to avoid orphan clicks, but stop when the click is released.
    * Changed some variable and function names to make sense with the code changes.
    * Added an extra function to deal with billboard mode. But is not perfect, specially with scaling and Y-billboard + camera tilting.

--- viewport/gui_in_3d/gui_3d.gd
+++ viewport/gui_in_3d/gui_3d.gd
@@ -1,102 +1,156 @@
 extends Spatial
-
 # Member variables
 # The size of the quad mesh itself.
-# NOTE: Do not apply the scale of the MeshInstance node, just the scale of the quad mesh!
-export (Vector2) var quad_mesh_size = Vector2(3, 2)
-# The scale of the quad node. It is assumed that the node is scaled evenly across the X, Y, and Z axes.
-export (float) var quad_node_scale = 1
-# The position of the last processed input touch/mouse event.
-var prev_pos = null
-# The last non-empty click_pos position. We need this to simulate drag events.
-var last_click_pos = null
-# The viewport we want to interact with.
-var viewport = null
-# A empty Vector3 used for comparison.
-var empty_vector = Vector3(0,0,0)
+var quad_mesh_size
+# Indentify if the mouse is inside the Area
+var mouse_inside = false
+# Identify if the mouse was pressed inside the Area
+var mouse_held = false
+# The last non-empty mouse position. Used when dragging outside of the box.
+var last_mouse_pos3D = null
+# The last processed input touch/mouse event. To calculate relative movement.
+var last_mouse_pos2D = null
+# Most used nodes
+onready var node_viewport = $Viewport
+onready var node_quad = $Quad
+onready var node_area = $Quad/Area
+onready var node_collision = $Quad/Area/CollisionShape
 
+func _ready():
+	node_area.connect("mouse_entered", self, "_mouse_entered_area")
+	print(node_quad.get_surface_material(0).params_billboard_mode)
 
+func _process(delta):
+	#NOTE: Remove this function if you don't plan on using billboard settings.
+	rotate_area_to_billboard()
+
+func _mouse_entered_area():
+	mouse_inside = true
+	
 func _input(event):
 	# Check if the event is a non-mouse/non-touch event
 	var is_mouse_event = false
-	var mouse_events = [InputEventMouseButton, InputEventMouseMotion, InputEventScreenDrag, InputEventScreenTouch]
-	for mouse_event in mouse_events:
+	for mouse_event in [InputEventMouseButton, InputEventMouseMotion, InputEventScreenDrag, InputEventScreenTouch]:
 		if event is mouse_event:
 			is_mouse_event = true
 			break
-  
+	
 	# If the event is not a mouse/touch event, then pass the event to the viewport as we do not
 	# need to do any conversions for these events.
-	if is_mouse_event == false:
-		viewport.input(event)
+	if is_mouse_event and (mouse_inside or mouse_held):
+		handle_mouse(event)
+	elif not is_mouse_event:
+		node_viewport.input(event)
 
-
-# Mouse events for Area
-func _on_area_input_event(camera, event, click_pos, click_normal, shape_idx):
-	# If click_pos is not empty, then we want to store it so we can use it to simulate drag events.
-	if click_pos != empty_vector:
-		last_click_pos = click_pos
+# Handle mouse events inside Area. (Area.input_event had many issues with dragging)
+func handle_mouse(event):
+	#Get mesh size to detect edges and make conversions. This code only support PlaneMesh and QuadMesh.
+	quad_mesh_size = node_quad.mesh.size
 	
-	var pos
-	if click_pos == empty_vector:
-		# Convert the last known click pos, last_click_pos, from world coordinate space to a coordinate space
-		# relative to the Area node.
-		# NOTE: affine_inverse accounts for the Area node's scale, rotation, and translation in the scene!
-		pos = get_node("Area").global_transform.affine_inverse()*last_click_pos
-		
-		# If the event is has some form of dragging, then we need to simulate that drag in code.
-		# NOTE: this is not a perfect solution, but it works okay.
-		if event is InputEventMouseMotion or event is InputEventScreenDrag:
-			pos.x += event.relative.x / viewport.size.x
-			pos.y -= event.relative.y / viewport.size.y
-		
-		# Update last_click_pos with the newest version of pos, with adjustments for quad size.
-		last_click_pos = pos * quad_node_scale
-		
-	else:
+	#Detect mouse being held to mantain event while outside of bounds. Avoid orphan clicks
+	if event is InputEventMouseButton or event is InputEventScreenTouch:
+		mouse_held = event.pressed
+	
+	#Find mouse position in Area
+	var mouse_pos3D = find_mouse(event.global_position)
+	
+	# Check if the mouse is outside of bounds, use last position to avoid errors
+	#NOTE: mouse_exited signal was unrealiable in this situation
+	mouse_inside = mouse_pos3D != null
+	if mouse_inside:
 		# Convert click_pos from world coordinate space to a coordinate space relative to the Area node.
 		# NOTE: affine_inverse accounts for the Area node's scale, rotation, and translation in the scene!
-		pos = get_node("Area").global_transform.affine_inverse()*click_pos
+		mouse_pos3D = node_area.global_transform.affine_inverse() * mouse_pos3D
+		last_mouse_pos3D = mouse_pos3D
+	else:
+		mouse_pos3D = last_mouse_pos3D
+	
+	#TODO: adapt to bilboard mode or avoid completelly
 	
 	# convert the relative event position from 3D to 2D
-	pos = Vector2(pos.x, -pos.y)
+	var mouse_pos2D = Vector2(mouse_pos3D.x, -mouse_pos3D.y)
 	
 	# Right now the event position's range is the following: (-quad_size/2) -> (quad_size/2)
 	# We need to convert it into the following range: 0 -> quad_size
-	pos.x += quad_mesh_size.x/2
-	pos.y += quad_mesh_size.y/2
+	mouse_pos2D.x += quad_mesh_size.x / 2
+	mouse_pos2D.y += quad_mesh_size.y / 2
 	# Then we need to convert it into the following range: 0 -> 1
-	pos.x = pos.x/quad_mesh_size.x
-	pos.y = pos.y/quad_mesh_size.y
+	mouse_pos2D.x = mouse_pos2D.x / quad_mesh_size.x
+	mouse_pos2D.y = mouse_pos2D.y / quad_mesh_size.y
+	
 	# Finally, we convert the position to the following range: 0 -> viewport.size
-	pos.x = pos.x * viewport.size.x
-	pos.y = pos.y * viewport.size.y
+	mouse_pos2D.x = mouse_pos2D.x * node_viewport.size.x
+	mouse_pos2D.y = mouse_pos2D.y * node_viewport.size.y
 	# We need to do these conversions so the event's position is in the viewport's coordinate system.
 	
 	# Set the event's position and global position.
-	event.position = pos
-	event.global_position = pos
+	event.position = mouse_pos2D
+	event.global_position = mouse_pos2D
 	
 	# If the event is a mouse motion event...
 	if event is InputEventMouseMotion:
 		# If there is not a stored previous position, then we'll assume there is no relative motion.
-		if prev_pos == null:
+		if last_mouse_pos2D == null:
 			event.relative = Vector2(0, 0)
 		# If there is a stored previous position, then we'll calculate the relative position by subtracting
 		# the previous position from the new position. This will give us the distance the event traveled from prev_pos
 		else:
-			event.relative = pos - prev_pos
-	
-	# Update prev_pos with the position we just calculated.
-	prev_pos = pos
+			event.relative = mouse_pos2D - last_mouse_pos2D
+	# Update last_mouse_pos2D with the position we just calculated.
+	last_mouse_pos2D = mouse_pos2D
 	
 	# Finally, send the processed input event to the viewport.
-	viewport.input(event)
+	node_viewport.input(event)
 
+func find_mouse(global_position):
+	var camera = get_viewport().get_camera()
+	
+	#from camera center to the mouse position in the Area
+	var from = camera.project_ray_origin(global_position)
+	var dist = find_further_distance_to(camera.transform.origin)
+	var to = from + camera.project_ray_normal(global_position) * dist
+	
+	
+	#Manually raycasts the are to find the mouse position
+	var result = get_world().direct_space_state.intersect_ray(from, to, [], node_area.collision_layer) #,false,true) #for 3.1 changes
+	
+	if result.size() > 0:
+		return result.position
+	else:
+		return null
 
-func _ready():
-	# Get the Viewport node and assign it to viewport for later use.
-	viewport = get_node("Viewport")
-	# Connect the input_event signal to the _on_area_input_event function.
-	get_node("Area").connect("input_event", self, "_on_area_input_event")
-  
+func find_further_distance_to(origin):
+	#Find edges of collision and change to global positions
+	var edges = []
+	edges.append(node_area.to_global(Vector3(quad_mesh_size.x / 2, quad_mesh_size.y / 2, 0)))
+	edges.append(node_area.to_global(Vector3(quad_mesh_size.x / 2, -quad_mesh_size.y / 2, 0)))
+	edges.append(node_area.to_global(Vector3(-quad_mesh_size.x / 2, quad_mesh_size.y / 2, 0)))
+	edges.append(node_area.to_global(Vector3(-quad_mesh_size.x / 2, -quad_mesh_size.y / 2, 0)))
+	
+	#Get the furthest distance between the camera and collision to avoid raycasting too far or too short
+	var far_dist = 0
+	var temp_dist
+	for edge in edges:
+		temp_dist = origin.distance_to(edge)
+		if temp_dist > far_dist:
+			far_dist = temp_dist
+	
+	return far_dist
+
+func rotate_area_to_billboard():
+	var billboard_mode = node_quad.get_surface_material(0).params_billboard_mode
+	
+	#try to match the area with the material's billboard setting, if enabled
+	if billboard_mode > 0:
+		#Look in the same direction as the camera
+		var look = get_viewport().get_camera().to_global(Vector3(0,0,-100)) - get_viewport().get_camera().global_transform.origin
+		look = node_area.translation + look
+		
+		# Y-Billboard: Lock Y rotation, but gives bad results if the camera is tilted.
+		if billboard_mode == 2: 
+			look = Vector3(look.x,0,look.z)
+		
+		node_area.look_at(look, Vector3(0,1,0))
+		
+		#Ratate in the Z axis to compensate camera tilt
+		node_area.rotate_object_local(Vector3(0,0,1), get_viewport().get_camera().rotation.z)
