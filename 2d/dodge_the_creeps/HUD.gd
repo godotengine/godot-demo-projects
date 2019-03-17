961c71d003a05af0618e549e3a76b48098bd5913
Chris Bradfield <cb@scribe.net>
2019-03-16 17:26:56 -0700

    Update Dodge demo to match 3.1 docs

--- 2d/dodge_the_creeps/HUD.gd
+++ 2d/dodge_the_creeps/HUD.gd
@@ -10,9 +10,10 @@ func show_message(text):
 func show_game_over():
 	show_message("Game Over")
 	yield($MessageTimer, "timeout")
-	$StartButton.show()
-	$MessageLabel.text = "Dodge the\nCreeps!"
+	$MessageLabel.text = "Dodge the\nCreeps"
 	$MessageLabel.show()
+	yield(get_tree().create_timer(1), 'timeout')
+	$StartButton.show()
 
 func update_score(score):
 	$ScoreLabel.text = str(score)
