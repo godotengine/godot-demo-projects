ceefc33fe49a80bc7b517607eb8ca78adbe40cb6
Juan Linietsky <reduzio@gmail.com>
2019-04-27 15:26:48 -0300

    Improved tutorial for sync audio with music, use two approaches.

--- audio/bpm_sync/Control.gd
+++ audio/bpm_sync/Control.gd
@@ -7,18 +7,34 @@ var playing = false
 const COMPENSATE_FRAMES = 2
 const COMPENSATE_HZ = 60.0
 
+const SYNC_SOURCE_SYSTEM_CLOCK = 0
+const SYNC_SOURCE_SOUND_CLOCK = 1
+
+var sync_source = SYNC_SOURCE_SYSTEM_CLOCK
+
+
 func strsec(secs):
 	var s = str(secs)
 	if (s.length()==1):
 		s="0"+s
 	return s
 	
-# warning-ignore:unused_argument
+# used by system clock
+var time_begin 
+var time_delay
+
 func _process(delta):
 	if (!playing or !$Player.playing):
 		return
 	
-	var time = $Player.get_mix_time() + AudioServer.get_time_since_last_mix() - AudioServer.get_output_latency() + (1/COMPENSATE_HZ)*COMPENSATE_FRAMES
+	var time 
+	if (sync_source == SYNC_SOURCE_SYSTEM_CLOCK): 
+		# obtain from ticks
+		time = (OS.get_ticks_usec() - time_begin) / 1000000.0
+		# compensate
+		time -= time_delay		
+	elif (sync_source == SYNC_SOURCE_SOUND_CLOCK):		
+		time = $Player.get_playback_position() + AudioServer.get_time_since_last_mix() - AudioServer.get_output_latency() + (1/COMPENSATE_HZ)*COMPENSATE_FRAMES
 		
 	var beat = int(time * BPM / 60.0)
 	var seconds = int(time)
@@ -26,8 +42,18 @@ func _process(delta):
 	$Label.text = str("BEAT: ",beat % BARS +1,"/",BARS," TIME: ",seconds/60,":",strsec(seconds%60)," / ",seconds_total/60,":",strsec(seconds_total%60))
 	
 
-func _on_Button_pressed():
-	print(AudioServer.get_output_latency())
+
+func _on_PlaySystem_pressed():
+	pass # Replace with function body.
+	sync_source = SYNC_SOURCE_SYSTEM_CLOCK
+	time_begin = OS.get_ticks_usec()
+	time_delay = AudioServer.get_time_to_next_mix() + AudioServer.get_output_latency()
 	playing=true
 	$Player.play()
 
+
+func _on_PlaySound_pressed():
+	sync_source = SYNC_SOURCE_SOUND_CLOCK
+	playing=true
+	$Player.play()
+	
