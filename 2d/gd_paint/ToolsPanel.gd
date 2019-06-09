7ad43829ef12eebe6b236b7c9b080b44fea98e19
TwistedTwigleg <beard.noah@gmail.com>
2019-06-09 18:44:01 -0400

    Updated project to work with the latest version of Godot. Reworked the code so it is cleaner and easier to understand. Updated comments within code according to changes

--- 2d/gd_paint/ToolsPanel.gd
+++ 2d/gd_paint/ToolsPanel.gd
@@ -12,7 +12,6 @@ var save_dialog
 
 
 func _ready():
-
 	# Get PaintControl and SaveFileDialog
 	paint_control = get_parent().get_node("PaintControl")
 	save_dialog = get_parent().get_node("SaveFileDialog")
@@ -27,7 +26,7 @@ func _ready():
 	get_node("ButtonToolEraser").connect("pressed", self, "button_pressed", ["mode_eraser"])
 	get_node("ButtonToolRectangle").connect("pressed", self, "button_pressed", ["mode_rectangle"])
 	get_node("ButtonToolCircle").connect("pressed", self, "button_pressed", ["mode_circle"])
-	get_node("ButtonShapeBox").connect("pressed", self, "button_pressed", ["shape_box"])
+	get_node("ButtonShapeBox").connect("pressed", self, "button_pressed", ["shape_rectangle"])
 	get_node("ButtonShapeCircle").connect("pressed", self, "button_pressed", ["shape_circle"])
 
 	# Assign all of the needed signals for the other brush settings (and ColorPickerBackground)
@@ -50,44 +49,55 @@ func _ready():
 
 func _physics_process(delta):
 	# Update the status label with the newest brush element count
-	label_stats.text = "Brush objects: " + String(paint_control.draw_elements_list.size())
+	label_stats.text = "Brush objects: " + String(paint_control.brush_data_list.size())
 
 
 func button_pressed(button_name):
-
 	# If a brush mode button is pressed
+	var tool_name = null
+	var shape_name = null
+	
 	if button_name == "mode_pencil":
-		paint_control.brush_mode = "pencil"
+		paint_control.brush_mode = paint_control.BRUSH_MODES.pencil
+		tool_name = "pencil"
 	elif button_name == "mode_eraser":
-		paint_control.brush_mode = "eraser"
+		paint_control.brush_mode = paint_control.BRUSH_MODES.eraser
+		tool_name = "eraser"
 	elif button_name == "mode_rectangle":
-		paint_control.brush_mode = "rectangle shape"
+		paint_control.brush_mode = paint_control.BRUSH_MODES.rectangle_shape
+		tool_name = "rectangle shape"
 	elif button_name == "mode_circle":
-		paint_control.brush_mode = "circle shape"
+		paint_control.brush_mode = paint_control.BRUSH_MODES.circle_shape
+		tool_name = "circle shape"
 
 	# If a brush shape button is pressed
-	elif button_name == "shape_box":
-		paint_control.brush_shape = "box"
+	elif button_name == "shape_rectangle":
+		paint_control.brush_shape = paint_control.BRUSH_SHAPES.rectangle
+		shape_name = "rectangle"
 	elif button_name == "shape_circle":
-		paint_control.brush_shape = "circle"
+		paint_control.brush_shape = paint_control.BRUSH_SHAPES.circle
+		shape_name = "circle";
 
 	# If a opperation button is pressed
 	elif button_name == "clear_picture":
-		paint_control.draw_elements_list = []
+		paint_control.brush_data_list = []
 		paint_control.update()
 	elif button_name == "save_picture":
 		save_dialog.popup_centered()
 	elif button_name == "undo_stroke":
 		paint_control.undo_stroke()
-
+	
 	# Update the labels (in case the brush mode or brush shape has changed)
-	label_tools.text = "Selected tool: " + paint_control.brush_mode
-	label_brush_shape.text = "Brush shape: " + paint_control.brush_shape
+	if tool_name != null:
+		label_tools.text = "Selected tool: " + tool_name
+	if shape_name != null:
+		label_brush_shape.text = "Brush shape: " + shape_name
 
 
 func brush_color_changed(color):
 	# Change the brush color to whatever color the color picker is
 	paint_control.brush_color = color
+
 
 func background_color_changed(color):
 	# Change the background color to whatever colorthe background color picker is
@@ -96,10 +106,12 @@ func background_color_changed(color):
 	# Because of how the eraser works we also need to redraw the paint control
 	paint_control.update()
 
+
 func brush_size_changed(value):
 	# Change the size of the brush, and update the label to reflect the new value
 	paint_control.brush_size = ceil(value)
 	label_brush_size.text = "Brush size: " + String(ceil(value)) + "px"
+
 
 func save_file_selected(path):
 	# Call save_picture in paint_control, passing in the path we recieved from SaveFileDialog
