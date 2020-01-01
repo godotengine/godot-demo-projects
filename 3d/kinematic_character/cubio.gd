998f1fe7e2e5a689d2b80d8fcd85cccafd621d97
nonunknown <58845030+nonunknown@users.noreply.github.com>
2020-01-01 13:37:03 -0300

    Add Look towards moving direction
    the look_system itself was developed by me, but lerp_angle and short_angle i've got on the internet forums
    
    I think this will help a lot of people to get their job done fast!!!

--- 3d/kinematic_character/cubio.gd
+++ 3d/kinematic_character/cubio.gd
@@ -10,9 +10,9 @@ const ACCEL= 2
 const DEACCEL= 4
 const MAX_SLOPE_ANGLE = 30
 
+var dir = Vector3() # Where does the player intend to walk to
 
 func _physics_process(delta):
-	var dir = Vector3() # Where does the player intend to walk to
 	var cam_xform = $target/camera.get_global_transform()
 
 	if Input.is_action_pressed("move_forward"):
@@ -48,7 +48,26 @@ func _physics_process(delta):
 
 	if is_on_floor() and Input.is_action_pressed("jump"):
 		vel.y = JUMP_SPEED
+		
+	look_system() # System to look at direction that player is moving
 
+var new_dir:Vector3 = Vector3.ZERO
+var new_angle:float = 0
+func look_system():
+	if (dir != Vector3.ZERO):
+		new_dir = dir
+	var angle:float = atan2(new_dir.x,new_dir.z) #get the angle from the player's input ie: 1,1.5,3
+	new_angle = lerp_angle(new_angle,angle,.25) # smooth lerp to the new angle (using custom method to avoid weirdo rotations)
+	var deg = rad2deg(new_angle) #convert the angle to degrees ie: 360,180,0,270
+	rotation_degrees.y = deg #apply to kinecnatic player's rotation
+
+func lerp_angle(from, to, weight):
+	return from + short_angle_dist(from, to) * weight
+
+func short_angle_dist(from, to):
+	var max_angle = PI * 2
+	var difference = fmod(to - from, max_angle)
+	return fmod(2 * difference, max_angle) - difference
 
 func _on_tcube_body_enter(body):
 	if body == self:
