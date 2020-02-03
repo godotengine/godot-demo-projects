b0e1cc022711a31bd550da75fca5948c9989ca52
Aaron Franke <arnfranke@yahoo.com>
2020-02-03 02:36:35 -0500

    Update 2D navigation demos

--- 2d/navigation/navigation.gd
+++ 2d/navigation/navigation.gd
@@ -1,46 +1,44 @@
 extends Navigation2D
 
-export(float) var CHARACTER_SPEED = 400.0
+export(float) var character_speed = 400.0
 var path = []
 
+func _process(delta):
+	var walk_distance = character_speed * delta
+	move_along_path(walk_distance)
+
+
 # The 'click' event is a custom input action defined in
-# Project > Project Settings > Input Map tab
+# Project > Project Settings > Input Map tab.
 func _input(event):
-	if not event.is_action_pressed('click'):
+	if not event.is_action_pressed("click"):
 		return
 	_update_navigation_path($Character.position, get_local_mouse_position())
-
-
-func _update_navigation_path(start_position, end_position):
-	# get_simple_path is part of the Navigation2D class
-	# it returns a PoolVector2Array of points that lead you from the
-	# start_position to the end_position
-	path = get_simple_path(start_position, end_position, true)
-	# The first point is always the start_position
-	# We don't need it in this example as it corresponds to the character's position
-	path.remove(0)
-	set_process(true)
-
-
-func _process(delta):
-	var walk_distance = CHARACTER_SPEED * delta
-	move_along_path(walk_distance)
 
 
 func move_along_path(distance):
 	var last_point = $Character.position
 	while path.size():
 		var distance_between_points = last_point.distance_to(path[0])
-
-		# the position to move to falls between two points
+		# The position to move to falls between two points.
 		if distance <= distance_between_points:
 			$Character.position = last_point.linear_interpolate(path[0], distance / distance_between_points)
 			return
-
-		# the position is past the end of the segment
+		# The position is past the end of the segment.
 		distance -= distance_between_points
 		last_point = path[0]
 		path.remove(0)
-	# the character reached the end of the path
+	# The character reached the end of the path.
 	$Character.position = last_point
 	set_process(false)
+
+
+func _update_navigation_path(start_position, end_position):
+	# get_simple_path is part of the Navigation2D class.
+	# It returns a PoolVector2Array of points that lead you
+	# from the start_position to the end_position.
+	path = get_simple_path(start_position, end_position, true)
+	# The first point is always the start_position.
+	# We don't need it in this example as it corresponds to the character's position.
+	path.remove(0)
+	set_process(true)
--- 2d/navigation_astar/character.gd
+++ 2d/navigation_astar/character.gd
@@ -1,8 +1,8 @@
 extends Position2D
 
-export(float) var SPEED = 200.0
+enum States { IDLE, FOLLOW }
 
-enum STATES { IDLE, FOLLOW }
+export(float) var speed = 200.0
 var _state = null
 
 var path = []
@@ -12,38 +12,35 @@ var target_position = Vector2()
 var velocity = Vector2()
 
 func _ready():
-	_change_state(STATES.IDLE)
-
-
-func _change_state(new_state):
-	if new_state == STATES.FOLLOW:
-		path = get_parent().get_node('TileMap')._get_path(position, target_position)
-		if not path or len(path) == 1:
-			_change_state(STATES.IDLE)
-			return
-		# The index 0 is the starting cell
-		# we don't want the character to move back to it in this example
-		target_point_world = path[1]
-	_state = new_state
+	_change_state(States.IDLE)
 
 
 func _process(_delta):
-	if not _state == STATES.FOLLOW:
+	if not _state == States.FOLLOW:
 		return
 	var arrived_to_next_point = move_to(target_point_world)
 	if arrived_to_next_point:
 		path.remove(0)
 		if len(path) == 0:
-			_change_state(STATES.IDLE)
+			_change_state(States.IDLE)
 			return
 		target_point_world = path[0]
+
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		if Input.is_key_pressed(KEY_SHIFT):
+			global_position = get_global_mouse_position()
+		else:
+			target_position = get_global_mouse_position()
+		_change_state(States.FOLLOW)
 
 
 func move_to(world_position):
 	var MASS = 10.0
 	var ARRIVE_DISTANCE = 10.0
 
-	var desired_velocity = (world_position - position).normalized() * SPEED
+	var desired_velocity = (world_position - position).normalized() * speed
 	var steering = desired_velocity - velocity
 	velocity += steering / MASS
 	position += velocity * get_process_delta_time()
@@ -51,10 +48,13 @@ func move_to(world_position):
 	return position.distance_to(world_position) < ARRIVE_DISTANCE
 
 
-func _input(event):
-	if event.is_action_pressed('click'):
-		if Input.is_key_pressed(KEY_SHIFT):
-			global_position = get_global_mouse_position()
-		else:
-			target_position = get_global_mouse_position()
-		_change_state(STATES.FOLLOW)
+func _change_state(new_state):
+	if new_state == States.FOLLOW:
+		path = get_parent().get_node("TileMap")._get_path(position, target_position)
+		if not path or len(path) == 1:
+			_change_state(States.IDLE)
+			return
+		# The index 0 is the starting cell
+		# we don't want the character to move back to it in this example
+		target_point_world = path[1]
+	_state = new_state
