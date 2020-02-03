3eeba859b1bff2cb0987caa9ea13ebe54a5208d4
Aaron Franke <arnfranke@yahoo.com>
2020-02-03 03:08:00 -0500

    Update 2D physics platformer

--- 2d/physics_platformer/platform/moving_platform.gd
+++ 2d/physics_platformer/platform/moving_platform.gd
@@ -1,17 +1,14 @@
+class_name MovingPlatform
 extends Node2D
 
-class_name MovingPlatform
-
-# Member variables
 export var motion = Vector2()
 export var cycle = 1.0
 
 var accum = 0.0
 
-
 func _physics_process(delta):
-	accum += delta * (1.0 / cycle) * PI * 2.0
-	accum = fmod(accum, PI * 2.0)
+	accum += delta * (1.0 / cycle) * TAU
+	accum = fmod(accum, TAU)
 	
 	var d = sin(accum)
 	var xf = Transform2D()
