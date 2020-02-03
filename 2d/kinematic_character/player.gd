746928b623000cfab1cfcda1921654ff003867ff
Aaron Franke <arnfranke@yahoo.com>
2020-02-02 23:35:33 -0500

    Update kinematic character

--- 2d/kinematic_character/player.gd
+++ 2d/kinematic_character/player.gd
@@ -1,11 +1,6 @@
 extends KinematicBody2D
 
-# This demo shows how to build a kinematic controller.
-
-# Member variables
-const GRAVITY = 500.0 # pixels/second/second
-
-# Angle in degrees towards either side that the player can consider "floor"
+# Angle in degrees towards either side that the player can consider "floor".
 const FLOOR_ANGLE_TOLERANCE = 40
 const WALK_FORCE = 600
 const WALK_MIN_SPEED = 10
@@ -14,55 +9,42 @@ const STOP_FORCE = 1300
 const JUMP_SPEED = 200
 const JUMP_MAX_AIRBORNE_TIME = 0.2
 
-const SLIDE_STOP_VELOCITY = 1.0 # one pixel/second
-const SLIDE_STOP_MIN_TRAVEL = 1.0 # one pixel
+const SLIDE_STOP_VELOCITY = 1.0 # Pixels/second
+const SLIDE_STOP_MIN_TRAVEL = 1.0 # Pixels
 
 var velocity = Vector2()
 var on_air_time = 100
 var jumping = false
-
 var prev_jump_pressed = false
 
+onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	# Create forces
-	var force = Vector2(0, GRAVITY)
-
-	var walk_left = Input.is_action_pressed("move_left")
-	var walk_right = Input.is_action_pressed("move_right")
+	var force = Vector2(0, gravity)
+	var walk = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var jump = Input.is_action_pressed("jump")
 
-	var stop = true
-
-	if walk_left:
-		if velocity.x <= WALK_MIN_SPEED and velocity.x > -WALK_MAX_SPEED:
-			force.x -= WALK_FORCE
-			stop = false
-	elif walk_right:
-		if velocity.x >= -WALK_MIN_SPEED and velocity.x < WALK_MAX_SPEED:
-			force.x += WALK_FORCE
-			stop = false
-
-	if stop:
+	if (velocity.x <= WALK_MIN_SPEED and velocity.x > -WALK_MAX_SPEED) or (velocity.x >= -WALK_MIN_SPEED and velocity.x < WALK_MAX_SPEED):
+		force.x += WALK_FORCE * walk
+	
+	if abs(walk) < 0.5:
 		var vsign = sign(velocity.x)
 		var vlen = abs(velocity.x)
-
 		vlen -= STOP_FORCE * delta
 		if vlen < 0:
 			vlen = 0
-
 		velocity.x = vlen * vsign
 
-	# Integrate forces to velocity
+	# Integrate forces to velocity.
 	velocity += force * delta
-	# Integrate velocity into motion and move
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	# Integrate velocity into motion and move.
+	velocity = move_and_slide(velocity, Vector2.UP)
 
 	if is_on_floor():
 		on_air_time = 0
 
 	if jumping and velocity.y > 0:
-		# If falling, no longer jumping
+		# If falling, no longer jumping.
 		jumping = false
 
 	if on_air_time < JUMP_MAX_AIRBORNE_TIME and jump and not prev_jump_pressed and not jumping:
---
+++ 2d/kinematic_character/world.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+func _on_princess_body_enter(body):
+	# The name of this editor-generated callback is unfortunate.
+	if body.get_name() == "Player":
+		$WinText.show()
