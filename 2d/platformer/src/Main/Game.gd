bd6da6a6106dde7b6388df5401f7534d8d24f3e6
Nathan Lovato <nathan@gdquest.com>
2020-02-11 22:08:01 -0600

    Limit the window size

--- 2d/platformer/src/Main/Game.gd
+++ 2d/platformer/src/Main/Game.gd
@@ -8,6 +8,11 @@ extends Node
 onready var _pause_menu = $InterfaceLayer/PauseMenu
 
 
+func _init() -> void:
+	OS.min_window_size = OS.window_size
+	OS.max_window_size = OS.get_screen_size()
+
+
 func _input(event):
 	if event.is_action_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = not OS.window_fullscreen
