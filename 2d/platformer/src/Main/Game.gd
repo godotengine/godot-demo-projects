332cd3bb38f06f43721a9ce1df5266ade4b4dd68
Nathan Lovato <nathan@gdquest.com>
2020-02-12 07:48:21 -0600

    Remove return type hint

--- 2d/platformer/src/Main/Game.gd
+++ 2d/platformer/src/Main/Game.gd
@@ -8,7 +8,7 @@ extends Node
 onready var _pause_menu = $InterfaceLayer/PauseMenu
 
 
-func _init() -> void:
+func _init():
 	OS.min_window_size = OS.window_size
 	OS.max_window_size = OS.get_screen_size()
 
