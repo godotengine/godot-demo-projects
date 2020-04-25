afd99e5aed16ec5600d61b4c70289f2c99cff772
PouleyKetchoupp <pouleyketchoup@gmail.com>
2020-04-25 12:47:47 +0200

    Add Physics Tests project

---
+++ 3d/physics_tests/README.md
@@ -0,0 +1,18 @@
+# 3D Physics Tests
+
+This demo contains a series of tests for the 3D 
+physics engine.
+
+They can be used for different purpose:
+- Functional tests to check for regressions and 
+compare the behavior between physics engines
+- Performance tests to evaluate and compare 
+performance between physics engines
+
+Language: GDScript
+
+Renderer: GLES 2
+
+## Screenshots
+
+![Screenshot](screenshots/screenshot.png)
---
+++ 3d/physics_tests/test.gd
@@ -0,0 +1,36 @@
+class_name Test
+extends Node
+
+
+var _timer
+var _timer_started = false
+
+
+func start_timer(timeout):
+	if _timer == null:
+		_timer = Timer.new()
+		_timer.one_shot = true
+		add_child(_timer)
+		_timer.connect("timeout", self, "_on_timer_done")
+	else:
+		cancel_timer()
+	
+	_timer.start(timeout)
+	_timer_started = true
+	
+	return _timer
+
+
+func cancel_timer():
+	if _timer_started:
+		_timer.paused = true
+		_timer.emit_signal("timeout")
+		_timer.paused = false
+
+
+func is_timer_canceled():
+	return _timer.paused
+
+
+func _on_timer_done():
+	_timer_started = false
