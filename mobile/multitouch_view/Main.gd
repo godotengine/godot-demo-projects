f9c93b0d288c293c13e5c695c10349a331984189
pkruszynski <69171579+pkruszynski@users.noreply.github.com>
2020-08-04 00:20:11 +0100

    Fix for multitouch_view

--- mobile/multitouch_view/Main.gd
+++ mobile/multitouch_view/Main.gd
@@ -9,14 +9,14 @@ func _draw():
 	# Get the touch helper singleton.
 	var touch_helper = get_node("/root/TouchHelper")
 	# Draw every pointer as a circle.
-	for ptr_id in touch_helper.state.keys():
-		var pos = touch_helper.state[ptr_id]
-		var color = _get_color_for_ptr_id(ptr_id)
+	for ptr_index in touch_helper.state.keys():
+		var pos = touch_helper.state[ptr_index]
+		var color = _get_color_for_ptr_index(ptr_index)
 		color.a = 0.75
 		draw_circle(pos, 40.0, color)
 
 
 # Just a way of getting different colors.
-func _get_color_for_ptr_id(id):
-	var x = (id % 7) + 1
+func _get_color_for_ptr_index(index):
+	var x = (index % 7) + 1
 	return Color(float(bool(x & 1)), float(bool(x & 2)), float(bool(x & 4)))
