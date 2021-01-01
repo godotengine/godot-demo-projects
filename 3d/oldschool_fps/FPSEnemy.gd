ba0ec3d7f3857df8ea5aba84c051ee2ee9a54590
JJRWalker <jjrwalkerdev@gmail.com>
2021-01-01 16:31:24 +0000

    Added enemy manager, changed enemy behaviour when hp is less than or equal to zero
    - Enemy now has a die function which deactivates it and moves it to a location outside of the map
    - When dying, the enemy calls the handle_death function in the enemy manager that, if there are no enemies active, will spawn a new wave of enemies.

--- 3d/oldschool_fps/FPSEnemy.gd
+++ 3d/oldschool_fps/FPSEnemy.gd
@@ -2,24 +2,35 @@
 
 class_name FPSEnemy
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# for this examples enemies simply move in a random direction until they die, then they are cycled out
+
+# ENEMY PROPERTIES
+# EXPOSED
 export(float) var recovery_time = 0.2;
-export(float) var max_hp = 100;
+export(float) var max_hp = 40;
 export(float) var decel_multiplier = 0.8;
 export(Color) var hit_colour = Color(1,0,0,1);
 
-var recovery_timer;
 var base_colour;
-var hp = 100;
+var hp = max_hp;
 var velocity = Vector3();
+var active = false;
+
+# SCENE REFS
+var recovery_timer;
 var enemy_sprite;
+var enemy_manager;
+
+# CONSTANTS
+const MIN_SPEED_BEFORE_CHANGING_DIR = 0.001;
+const ENEMY_SPEED_RANGE = 1000;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	enemy_sprite = get_node("Sprite3D");
 	recovery_timer = get_node("RecoveryTimer");
+	enemy_manager = get_tree().get_current_scene().get_node("EnemyManager");
+	
 	base_colour = enemy_sprite.modulate;
 
 
@@ -29,14 +40,27 @@ func take_damage(var damage, var direction, var knockback):
 	# react to the damage
 	enemy_sprite.modulate = hit_colour;
 	velocity += -direction.normalized() * knockback;
+	
+	if (hp <= 0):
+		_die();
+		return;
+	
 	recovery_timer.start(recovery_time);
 
 
 func _physics_process(delta):
-	if (velocity.length() < 0.001): #TODO : change this not to be hardcoded vadiables ( use a const var with a name for this like you did for other stuff )
-		velocity = Vector3(rand_range(-1000,1000), 0, rand_range(-1000,1000));  #TODO : change this not to be hardcoded vadiables ( use a const var with a name for this like you did for other stuff )
-	velocity *= decel_multiplier;
-	apply_central_impulse(velocity * delta);
+	if (active):
+		if (velocity.length() < MIN_SPEED_BEFORE_CHANGING_DIR):
+			velocity = Vector3(rand_range(-ENEMY_SPEED_RANGE,ENEMY_SPEED_RANGE), 0, rand_range(-ENEMY_SPEED_RANGE,ENEMY_SPEED_RANGE));
+		velocity *= decel_multiplier;
+		apply_central_impulse(velocity * delta);
+
+# pooling deactivation 
+func _die():
+	set_translation(enemy_manager.DEFAULT_ENEMY_SPAWN_POINT);
+	active = false;
+	hp = max_hp;
+	enemy_manager.handle_death();
 
 
 func _on_RecoveryTimer_timeout():
---
+++ 3d/oldschool_fps/FPSEnemyManager.gd
@@ -0,0 +1,50 @@
+extends Node
+
+class_name FPSEnemyManager
+
+# class cycles through spawn points and pooled enemies, spawns a specified number of enemies at the start
+# then when they're all killed, it spawns another wave
+
+export(int) var starting_num_of_enemies = 1;
+
+var enemies_in_level;
+var spawn_locations;
+
+var spawn_location_index = 0;
+
+var num_of_enemies_in_wave = starting_num_of_enemies;
+
+const DEFAULT_ENEMY_SPAWN_POINT = Vector3(0, -100, 0);
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	spawn_locations = get_node("SpawnPoints").get_children();
+	enemies_in_level = get_node("Enemies").get_children();
+	
+	for i in starting_num_of_enemies:
+		enemies_in_level[i].set_translation(spawn_locations[i % spawn_locations.size() - 1].global_transform.origin);
+		enemies_in_level[i].active = true;
+	
+	spawn_location_index = starting_num_of_enemies - 1;
+	
+
+# if one enemy is still active this will return out before spawning a new wave
+func handle_death():
+	for i in enemies_in_level.size():
+		if (enemies_in_level[i].active):
+			return;	
+	spawn_new_enemy_wave();
+	
+
+func spawn_new_enemy_wave():
+	num_of_enemies_in_wave += 1;
+	if (num_of_enemies_in_wave == enemies_in_level.size()):
+		num_of_enemies_in_wave = 1;
+	for i in num_of_enemies_in_wave:
+		if (spawn_location_index == spawn_locations.size()):
+			spawn_location_index = 0
+		enemies_in_level[i].set_translation(spawn_locations[spawn_location_index].global_transform.origin);
+		enemies_in_level[i].active = true;
+		spawn_location_index += 1;
+
