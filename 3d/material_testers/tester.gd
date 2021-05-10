cfba5915b4c0d29b1f9309fe0f843f7426593de7
Bastiaan Olij <mux213@gmail.com>
2021-05-10 14:13:59 +1000

    Porting material testers project to Godot 4

--- 3d/material_testers/tester.gd
+++ 3d/material_testers/tester.gd
@@ -4,7 +4,7 @@ const INTERP_SPEED = 2
 const ROT_SPEED = 0.003
 const ZOOM_SPEED = 0.1
 const ZOOM_MAX = 2.5
-const MAIN_BUTTONS = MOUSE_BUTTON_MASK_LEFT | MOUSE_BUTTON_MASK_RIGHT | MOUSE_BUTTON_MASK_MIDDLE
+const MAIN_BUTTONS = MOUSE_BUTTON_MASK_LEFT | MOUSE_BUTTON_MASK_MIDDLE | MOUSE_BUTTON_MASK_RIGHT
 
 var tester_index = 0
 var rot_x = -0.5 # This must be kept in sync with RotationX.
@@ -20,12 +20,12 @@ var backgrounds = [
 	{ path = "res://backgrounds/experiment.hdr", name = "Experiment"},
 ]
 
-@onready var testers = $Testers
-@onready var material_name = $UI/MaterialName
+@onready var testers: Node3D = $Testers
+@onready var material_name: Label = $UI/MaterialName
 
-@onready var camera_holder = $CameraHolder # Has a position and rotates on Y.
-@onready var rotation_x = $CameraHolder/RotationX
-@onready var camera = $CameraHolder/RotationX/Camera3D
+@onready var camera_holder: Node3D = $CameraHolder # Has a position and rotates on Y.
+@onready var rotation_x: Node3D = $CameraHolder/RotationX
+@onready var camera: Camera3D = $CameraHolder/RotationX/Camera
 
 func _ready():
 	for background in backgrounds:
@@ -48,8 +48,8 @@ func _unhandled_input(ev):
 		rot_x -= relative_motion.y * ROT_SPEED
 		rot_y = clamp(rot_y, -1.6, 1.6)
 		rot_x = clamp(rot_x, -1.4, 0.5)
-		camera_holder.transform.basis = Basis.from_euler(Vector3(0, rot_y, 0))
-		rotation_x.transform.basis = Basis.from_euler(Vector3(rot_x, 0, 0))
+		camera_holder.transform.basis = Basis().looking_at(Vector3(0, rot_y, 0))
+		rotation_x.transform.basis = Basis().looking_at(Vector3(rot_x, 0, 0))
 
 
 func _process(delta):
@@ -59,6 +59,8 @@ func _process(delta):
 	var target_position = current_tester.transform.origin.x
 	var current_position = camera_holder.transform.origin.x
 	camera_holder.transform.origin.x = lerp(current_position, target_position, INTERP_SPEED * delta)
+
+	$UI/FPS.text = "FPS: " + str(Performance.get_monitor(Performance.TIME_FPS))
 
 
 func _on_Previous_pressed():
@@ -72,4 +74,10 @@ func _on_Next_pressed():
 
 
 func _on_bg_item_selected(index):
-	get_node(^"WorldEnvironment").environment.sky.sky_material.panorama = load(backgrounds[index].path)
+	var sky_material : PanoramaSkyMaterial = $WorldEnvironment.environment.sky.sky_material
+
+	sky_material.panorama = load(backgrounds[index].path)
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
