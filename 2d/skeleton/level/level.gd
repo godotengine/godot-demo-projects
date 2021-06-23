cd48c9a9cccaaf7058e0b4756f646cebe2f76409
Aaron Franke <arnfranke@yahoo.com>
2021-06-23 12:56:33 -0400

    Add a Skeleton2D demo

---
+++ 2d/skeleton/level/level.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const LIMIT_LEFT = -315
+const LIMIT_TOP = -250
+const LIMIT_RIGHT = 955
+const LIMIT_BOTTOM = 690
+
+func _ready():
+	for child in get_children():
+		if child is Player:
+			var camera = child.get_node(@"Camera2D")
+			camera.limit_left = LIMIT_LEFT
+			camera.limit_top = LIMIT_TOP
+			camera.limit_right = LIMIT_RIGHT
+			camera.limit_bottom = LIMIT_BOTTOM
