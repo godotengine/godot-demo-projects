21961880ebba3807e14292cbd0739b39ef37f5b8
alan-man <alan.tambellinijonic@gmail.com>
2022-01-02 15:19:29 -0800

    Add a 3D Rigidbody character demo
    https://github.com/godotengine/godot-demo-projects/issues/669
    Add a 3D Rigidbody character demo.

---
+++ 3d/rigidbody_character/level.gd
@@ -0,0 +1,14 @@
+extends Spatial
+
+func _ready():
+	pass
+
+
+# Random spawn of Rigidbody cubes. 
+func _on_SpawnTimer_timeout():
+	var rb = preload("res://cubeRB.tscn")
+	var new_rb = rb.instance()
+	new_rb.translation.y = 15
+	new_rb.translation.x = rand_range(-5, 5)
+	new_rb.translation.z = rand_range(-5, 5)
+	add_child(new_rb)
