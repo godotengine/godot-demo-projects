c22732e4cc7f617215778f61b3d0cf1de2dc8c55
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-07-15 18:55:05 +0200

    Improve 3D global illumination demo
    - Readd LightmapGI example.
    - Allow toggling between the Once and Always update modes for the
      ReflectionProbe.
    - Add a Decal node to act as a blob shadow for dynamic objects.
    - Halve SSAO intensity for a better apperance in the demo's flat,
      bright surfaces.
    - Halve ambient light sky contribution when using the disabled
      or lightmap-based GI modes. This prevents the level's shaded areas
      from looking too bright and blue (with lightmaps, this change only
      affects dynamic objects.)
    - Enable FXAA and debanding for a better appearance.

--- 3d/global_illumination/test.gd
+++ 3d/global_illumination/test.gd
@@ -2,8 +2,8 @@ extends Node3D
 
 enum GIMode {
 	NONE,
-#	BAKED_LIGHTMAP_ALL,
-#	BAKED_LIGHTMAP_INDIRECT,
+	LIGHTMAP_GI_ALL,
+	LIGHTMAP_GI_INDIRECT,
 	VOXEL_GI,
 	SDFGI,
 	MAX,  # Maximum value of the enum, used internally.
@@ -12,10 +12,24 @@ enum GIMode {
 # Keep this in sync with the GIMode enum (except for MAX).
 const GI_MODE_TEXTS = [
 	"Environment Lighting (Fastest)",
-#	"Baked Lightmap All (Fast)",
-#	"Baked Lightmap Indirect (Average)",
+	"Baked Lightmap All (Fast)",
+	"Baked Lightmap Indirect (Average)",
 	"VoxelGI (Slow)",
 	"SDFGI (Slow)",
+]
+
+enum ReflectionProbeMode {
+	NONE,
+	ONCE,
+	ALWAYS,
+	MAX,
+}
+
+# Keep this in sync with the ReflectionProbeMode enum (except for MAX).
+const REFLECTION_PROBE_MODE_TEXTS = [
+	"Disabled - Using environment, VoxelGI or SDFGI reflections (Fast)",
+	"Enabled - \"Once\" Update Mode (Average)",
+	"Enabled - \"Always\" Update Mode (Slow)",
 ]
 
 enum SSILMode {
@@ -35,7 +49,7 @@ const SSIL_MODE_TEXTS = [
 ]
 
 var gi_mode = GIMode.NONE
-var use_reflection_probe = false
+var reflection_probe_mode = ReflectionProbeMode.NONE
 var ssil_mode = SSILMode.NONE
 
 @onready var environment = $WorldEnvironment.environment
@@ -44,10 +58,15 @@ var ssil_mode = SSILMode.NONE
 @onready var reflection_probe = $Camera/ReflectiveSphere/ReflectionProbe
 @onready var ssil_mode_label = $SSILMode
 
+# Several copies of the level mesh are required to cycle between different GI modes.
+@onready var zdm2_no_lightmap = $Zdm2NoLightmap
+@onready var zdm2_lightmap_all = $Zdm2LightmapAll
+@onready var zdm2_lightmap_indirect = $Zdm2LightmapIndirect
+
 
 func _ready():
 	set_gi_mode(gi_mode)
-	set_use_reflection_probe(use_reflection_probe)
+	set_reflection_probe_mode(reflection_probe_mode)
 	set_ssil_mode(ssil_mode)
 
 
@@ -55,8 +74,8 @@ func _input(event):
 	if event.is_action_pressed("cycle_gi_mode"):
 		set_gi_mode(wrapi(gi_mode + 1, 0, GIMode.MAX))
 
-	if event.is_action_pressed("toggle_reflection_probe"):
-		set_use_reflection_probe(not use_reflection_probe)
+	if event.is_action_pressed("cycle_reflection_probe_mode"):
+		set_reflection_probe_mode(wrapi(reflection_probe_mode + 1, 0, ReflectionProbeMode.MAX))
 
 	if event.is_action_pressed("cycle_ssil_mode"):
 		set_ssil_mode(wrapi(ssil_mode + 1, 0, SSILMode.MAX))
@@ -68,8 +87,14 @@ func set_gi_mode(p_gi_mode):
 
 	match p_gi_mode:
 		GIMode.NONE:
-#			$BakedLightmapIndirect.visible = false
-#			$BakedLightmapAll.visible = false
+			$Zdm2NoLightmap.visible = true
+			$Zdm2LightmapAll.visible = false
+			$Zdm2LightmapIndirect.visible = false
+
+			# Halve sky contribution to prevent shaded areas from looking too bright and blue.
+			environment.ambient_light_sky_contribution = 0.5
+			$LightmapGIIndirect.visible = false
+			$LightmapGIAll.visible = false
 			$VoxelGI.visible = false
 			environment.sdfgi_enabled = false
 
@@ -80,71 +105,94 @@ func set_gi_mode(p_gi_mode):
 			$GarageOmniLight.light_bake_mode = Light3D.BAKE_DYNAMIC
 			$CornerSpotLight.light_bake_mode = Light3D.BAKE_DYNAMIC
 
-#		GIMode.BAKED_LIGHTMAP_ALL:
-#			$BakedLightmapIndirect.visible = false
-#			$BakedLightmapAll.visible = true
-#			$VoxelGI.visible = false
-#			environment.sdfgi_enabled = false
-#
-#			# Make lights not affect baked surfaces by setting their bake mode to All.
-#			$Sun.light_bake_mode = Light3D.BAKE_STATIC
-#			$GrateOmniLight.light_bake_mode = Light3D.BAKE_STATIC
-#			$GarageOmniLight.light_bake_mode = Light3D.BAKE_STATIC
-#			$CornerSpotLight.light_bake_mode = Light3D.BAKE_STATIC
+		GIMode.LIGHTMAP_GI_ALL:
+			$Zdm2NoLightmap.visible = false
+			$Zdm2LightmapAll.visible = true
+			$Zdm2LightmapIndirect.visible = false
 
-#		GIMode.BAKED_LIGHTMAP_INDIRECT:
-#			$BakedLightmapIndirect.visible = true
-#			$BakedLightmapAll.visible = false
-#			$VoxelGI.visible = false
-#			environment.sdfgi_enabled = false
-#
-#			$Sun.light_bake_mode = Light3D.BAKE_DYNAMIC
-#			$GrateOmniLight.light_bake_mode = Light3D.BAKE_DYNAMIC
-#			$GarageOmniLight.light_bake_mode = Light3D.BAKE_DYNAMIC
-#			$CornerSpotLight.light_bake_mode = Light3D.BAKE_DYNAMIC
+			# Halve sky contribution to prevent dynamic objects from looking too bright and blue.
+			# (When using lightmaps, this property doesn't affect lightmapped surfaces.)
+			environment.ambient_light_sky_contribution = 0.5
+			$LightmapGIIndirect.visible = false
+			$LightmapGIAll.visible = true
+			$VoxelGI.visible = false
+			environment.sdfgi_enabled = false
+
+			# Make lights not affect baked surfaces by setting their bake mode to All.
+			$Sun.light_bake_mode = Light3D.BAKE_STATIC
+			$GrateOmniLight.light_bake_mode = Light3D.BAKE_STATIC
+			$GarageOmniLight.light_bake_mode = Light3D.BAKE_STATIC
+			$CornerSpotLight.light_bake_mode = Light3D.BAKE_STATIC
+
+		GIMode.LIGHTMAP_GI_INDIRECT:
+			$Zdm2NoLightmap.visible = false
+			$Zdm2LightmapAll.visible = false
+			$Zdm2LightmapIndirect.visible = true
+
+			# Halve sky contribution to prevent dynamic objects from looking too bright and blue.
+			# (When using lightmaps, this property doesn't affect lightmapped surfaces.)
+			environment.ambient_light_sky_contribution = 0.5
+			$LightmapGIIndirect.visible = true
+			$LightmapGIAll.visible = false
+			$VoxelGI.visible = false
+			environment.sdfgi_enabled = false
+
+			$Sun.light_bake_mode = Light3D.BAKE_DYNAMIC
+			$GrateOmniLight.light_bake_mode = Light3D.BAKE_DYNAMIC
+			$GarageOmniLight.light_bake_mode = Light3D.BAKE_DYNAMIC
+			$CornerSpotLight.light_bake_mode = Light3D.BAKE_DYNAMIC
 
 		GIMode.VOXEL_GI:
-#			$BakedLightmapIndirect.visible = false
-#			$BakedLightmapAll.visible = false
+			$Zdm2NoLightmap.visible = true
+			$Zdm2LightmapAll.visible = false
+			$Zdm2LightmapIndirect.visible = false
+
+			environment.ambient_light_sky_contribution = 1.0
+			$LightmapGIIndirect.visible = false
+			$LightmapGIAll.visible = false
 			$VoxelGI.visible = true
 			environment.sdfgi_enabled = false
 
 			# Bake mode must be Indirect, not Disabled. Otherwise, GI will
 			# not be visible for those lights.
-			# Moving/blinking lights should generally have their bake mode set to Disabled
-			# to avoid visible GI pop-ins. This is because VoxelGI
-			# can take a while to update.
 			$Sun.light_bake_mode = Light3D.BAKE_DYNAMIC
 			$GrateOmniLight.light_bake_mode = Light3D.BAKE_DYNAMIC
 			$GarageOmniLight.light_bake_mode = Light3D.BAKE_DYNAMIC
 			$CornerSpotLight.light_bake_mode = Light3D.BAKE_DYNAMIC
 
 		GIMode.SDFGI:
-#			$BakedLightmapIndirect.visible = false
-#			$BakedLightmapAll.visible = false
+			$Zdm2NoLightmap.visible = true
+			$Zdm2LightmapAll.visible = false
+			$Zdm2LightmapIndirect.visible = false
+
+			environment.ambient_light_sky_contribution = 1.0
+			$LightmapGIIndirect.visible = false
+			$LightmapGIAll.visible = false
 			$VoxelGI.visible = false
 			environment.sdfgi_enabled = true
 
 			# Bake mode must be Indirect, not Disabled. Otherwise, GI will
 			# not be visible for those lights.
-			# Moving/blinking lights should generally have their bake mode set to Disabled
-			# to avoid visible GI pop-ins. This is because SDFGI
-			# can take a while to update.
 			$Sun.light_bake_mode = Light3D.BAKE_DYNAMIC
 			$GrateOmniLight.light_bake_mode = Light3D.BAKE_DYNAMIC
 			$GarageOmniLight.light_bake_mode = Light3D.BAKE_DYNAMIC
 			$CornerSpotLight.light_bake_mode = Light3D.BAKE_DYNAMIC
 
 
-func set_use_reflection_probe(p_visible):
-	use_reflection_probe = p_visible
+func set_reflection_probe_mode(p_reflection_probe_mode):
+	reflection_probe_mode = p_reflection_probe_mode
+	reflection_probe_mode_label.text = "Reflection probe: %s " % REFLECTION_PROBE_MODE_TEXTS[reflection_probe_mode]
 
-	if p_visible:
-		reflection_probe_mode_label.text = "Reflection probe: Enabled - Using reflection probe (Average)"
-	else:
-		reflection_probe_mode_label.text = "Reflection probe: Disabled - Using environment, VoxelGI or SDFGI reflections (Fast)"
-
-	reflection_probe.visible = p_visible
+	match p_reflection_probe_mode:
+		ReflectionProbeMode.NONE:
+			reflection_probe.visible = false
+			reflection_probe.update_mode = ReflectionProbe.UPDATE_ONCE
+		ReflectionProbeMode.ONCE:
+			reflection_probe.visible = true
+			reflection_probe.update_mode = ReflectionProbe.UPDATE_ONCE
+		ReflectionProbeMode.ALWAYS:
+			reflection_probe.visible = true
+			reflection_probe.update_mode = ReflectionProbe.UPDATE_ALWAYS
 
 
 func set_ssil_mode(p_ssil_mode):
