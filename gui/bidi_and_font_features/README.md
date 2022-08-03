c2626a564b439123281fd5fe93e1cab0b4da2d8b
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-08-04 01:21:20 +0200

    Update Bidi and Font Features demo for 4.0.rc4
    - Enable low processor mode and multiple resolution-friendly
      project settings for consistency with other demos.
    - Use Compatibility rendering method for better performance
      and compatibility.
    - Prevent screenshots from being imported as resources.

--- gui/bidi_and_font_features/README.md
+++ gui/bidi_and_font_features/README.md
@@ -1,13 +1,15 @@
-BiDI and Font Features Demo
+# BiDI and Font Features Demo
 
 A demo showing BiDi text and BiDi overrides, line breaking and justification, and OpenType font features.
 
 Language: GDScript
 
-Renderer: Vulkan
+Renderer: Compatibility
 
 ## Screenshots
 
 ![Screenshot](screenshots/bidi.png)
+
 ![Screenshot](screenshots/line_break_and_fill.png)
+
 ![Screenshot](screenshots/font_features.png)
--- gui/bidi_and_font_features/custom_st_parser.gd
+++ gui/bidi_and_font_features/custom_st_parser.gd
@@ -7,8 +7,8 @@ func _structured_text_parser(args, text):
 	var count = int(tags.size())
 	output.clear()
 	for i in range(count):
-		var range1 = Vector2i(prev, prev + tags[i].length())
-		var range2 = Vector2i(prev + tags[i].length(), prev + tags[i].length() + 1)
+		var range1 = Vector3i(prev, prev + tags[i].length(), TextServer.DIRECTION_AUTO)
+		var range2 = Vector3i(prev + tags[i].length(), prev + tags[i].length() + 1, TextServer.DIRECTION_AUTO)
 		output.push_front(range1)
 		output.push_front(range2)
 		prev = prev + tags[i].length() + 1
