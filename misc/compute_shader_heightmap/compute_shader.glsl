9e56cea2fb11e4cc49f5f329641c6830e11e1506
MoltenCoffee <13321277+MoltenCoffee@users.noreply.github.com>
2022-09-22 19:34:10 +0200

    Change workgroup size

--- misc/compute_shader_heightmap/compute_shader.glsl
+++ misc/compute_shader_heightmap/compute_shader.glsl
@@ -1,7 +1,8 @@
 #[compute]
 #version 460
 
-layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+// Instruct the GPU to use 8x8x1 = 64 local invocations per workgroup
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
 // Prepare memory for the image, which will be both read and written to
 // 'restrict' is used to tell the compiler that the memory will only be accessed by the 'heightmap' variable
