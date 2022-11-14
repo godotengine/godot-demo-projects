93509019a9a22b6223cf425d5cc9a0609cc7be2e
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2022-11-14 22:53:21 +0100

    Update WebRTC minimal to 4.0-beta4.

--- networking/webrtc_minimal/minimal.gd
+++ networking/webrtc_minimal/minimal.gd
@@ -10,26 +10,26 @@ var ch2 = p2.create_data_channel("chat", {"id": 1, "negotiated": true})
 func _ready():
 	print(p1.create_data_channel("chat", {"id": 1, "negotiated": true}))
 	# Connect P1 session created to itself to set local description.
-	p1.connect(&"session_description_created", p1.set_local_description)
+	p1.session_description_created.connect(p1.set_local_description)
 	# Connect P1 session and ICE created to p2 set remote description and candidates.
-	p1.connect(&"session_description_created", p2.set_remote_description)
-	p1.connect(&"ice_candidate_created", p2.add_ice_candidate)
+	p1.session_description_created.connect(p2.set_remote_description)
+	p1.ice_candidate_created.connect(p2.add_ice_candidate)
 
 	# Same for P2.
-	p2.connect(&"session_description_created", p2.set_local_description)
-	p2.connect(&"session_description_created", p1.set_remote_description)
-	p2.connect(&"ice_candidate_created", p1.add_ice_candidate)
+	p2.session_description_created.connect(p2.set_local_description)
+	p2.session_description_created.connect(p1.set_remote_description)
+	p2.ice_candidate_created.connect(p1.add_ice_candidate)
 
 	# Let P1 create the offer.
 	p1.create_offer()
 
 	# Wait a second and send message from P1.
 	await get_tree().create_timer(1).timeout
-	ch1.put_packet("Hi from P1".to_utf8())
+	ch1.put_packet("Hi from P1".to_utf8_buffer())
 
 	# Wait a second and send message from P2.
 	await get_tree().create_timer(1).timeout
-	ch2.put_packet("Hi from P2".to_utf8())
+	ch2.put_packet("Hi from P2".to_utf8_buffer())
 
 
 func _process(delta):
