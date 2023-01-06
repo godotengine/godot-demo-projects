fa356b2b7bc92fdf28602388c8712e9f20edd6ab
Zoee Silcock <mrzoee@gmail.com>
2023-01-06 13:25:41 +0100

    Use the new property name of the viewport width/height settings

--- gui/multiple_resolutions/main.gd
+++ gui/multiple_resolutions/main.gd
@@ -4,7 +4,10 @@
 # (with their rect spread across the whole viewport, and Anchor set to Full Rect).
 extends Control
 
-var base_window_size = Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height"))
+var base_window_size = Vector2(
+	ProjectSettings.get_setting("display/window/size/viewport_width"),
+	ProjectSettings.get_setting("display/window/size/viewport_height")
+)
 
 # These defaults match this demo's project settings. Adjust as needed if adapting this
 # in your own project.
