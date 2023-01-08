cf931b8308ec9b2fce95be2a5ee2c0562cce03fa
Cykyrios <cykyrios@gmail.com>
2023-01-09 00:25:09 +0100

    Update regex demo for Godot 4.0 beta 10

--- gui/regex/regex.gd
+++ gui/regex/regex.gd
@@ -3,8 +3,9 @@ extends VBoxContainer
 var regex = RegEx.new()
 
 func _ready():
-	$Text.set_text("They asked me \"What's going on \\\"in the manor\\\"?\"")
-	update_expression($Expression.text)
+	%Text.text = "The file is located in /tmp/test/index.html, and not in /x/y/z.html."
+	%Expression.text = "(/.+?\\.html).+?(/.+?\\.html)"
+	update_expression(%Expression.text)
 
 
 func update_expression(text):
@@ -13,12 +14,20 @@ func update_expression(text):
 
 
 func update_text():
-	for child in $List.get_children():
+	for child in %List.get_children():
 		child.queue_free()
-	if regex.is_valid():
-		var matches = regex.search($Text.get_text())
-		if matches != null:
-			for result in matches.get_strings():
-				var label = Label.new()
-				label.text = result
-				$List.add_child(label)
+	if not regex.is_valid():
+		return
+	var matches = regex.search_all(%Text.text)
+	if matches == null:
+		return
+	for i in matches.size():
+		var result = matches[i]
+		for j in result.strings.size():
+			var pattern = result.strings[j]
+			if pattern == "":
+				continue
+			var label = Label.new()
+			var match_text = "Match %d" % [i + 1] if j == 0 else "    Group %d" % [j]
+			label.text = "%s: %s" % [match_text, pattern]
+			%List.add_child(label)
