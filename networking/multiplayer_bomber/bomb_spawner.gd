c1461de661963a5e3f83beb3e402f3a61da9b14b
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2023-01-13 16:19:18 +0100

    [Net] Fix Multiplayer Bomber custom spawner.
    Needed starting beta11 (Godot GH-71129).

--- networking/multiplayer_bomber/bomb_spawner.gd
+++ networking/multiplayer_bomber/bomb_spawner.gd
@@ -1,6 +1,10 @@
 extends MultiplayerSpawner
 
-func _spawn_custom(data):
+func _init():
+	spawn_function = _spawn_bomb
+
+
+func _spawn_bomb(data):
 	if data.size() != 2 or typeof(data[0]) != TYPE_VECTOR2 or typeof(data[1]) != TYPE_INT:
 		return null
 	var bomb = preload("res://bomb.tscn").instantiate()
