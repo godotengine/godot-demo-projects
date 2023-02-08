26d2f226a805c622f14d0f526677f33e4f0b8c43
Justo Delgado <contact@mrcdk.com>
2023-02-08 18:25:55 +0100

    Fixed isometric demo rendering for Godot 4.0-rc1 (#840)

--- 2d/isometric/troll.gd
+++ 2d/isometric/troll.gd
@@ -4,9 +4,7 @@ const MOTION_SPEED = 30 # Pixels/second.
 const FRICTION_FACTOR = 0.89
 
 func _physics_process(_delta):
-	var motion = Vector2()
-	motion.x = Input.get_axis(&"move_left", &"move_right")
-	motion.y = Input.get_axis(&"move_up", &"move_down")
+	var motion = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	# Make diagonal movement fit isometric tiles.
 	motion.y /= 2
 	velocity += motion.normalized() * MOTION_SPEED
