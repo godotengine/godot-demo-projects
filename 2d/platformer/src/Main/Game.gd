534c8e196558271c2450c737fc44a9fd8bf8a4e3
huulong <n.huu.long@gmail.com>
2023-02-17 17:05:39 +0100

    [3to4] Platformer 2D: update API to Godot4 using `godot4 --convert-3to4` (with v4.0.rc2.official [d2699dc7a])
    Manually revert incorrect changes, namely 'on' -> 'checked' in comments

--- 2d/platformer/src/Main/Game.gd
+++ 2d/platformer/src/Main/Game.gd
@@ -18,8 +18,8 @@ func _notification(what):
 
 func _unhandled_input(event):
 	if event.is_action_pressed("toggle_fullscreen"):
-		OS.window_fullscreen = not OS.window_fullscreen
-		get_tree().set_input_as_handled()
+		get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN if (not ((get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN) or (get_window().mode == Window.MODE_FULLSCREEN))) else Window.MODE_WINDOWED
+		get_viewport().set_input_as_handled()
 	# The GlobalControls node, in the Stage scene, is set to process even
 	# when the game is paused, so this code keeps running.
 	# To see that, select GlobalControls, and scroll down to the Pause category
@@ -31,13 +31,13 @@ func _unhandled_input(event):
 			_pause_menu.open()
 		else:
 			_pause_menu.close()
-		get_tree().set_input_as_handled()
+		get_viewport().set_input_as_handled()
 
 	elif event.is_action_pressed("splitscreen"):
 		if name == "Splitscreen":
 			# We need to clean up a little bit first to avoid SubViewport errors.
 			$Black/SplitContainer/ViewportContainer1.free()
 			$Black.queue_free()
-			get_tree().change_scene("res://src/Main/Game.tscn")
+			get_tree().change_scene_to_file("res://src/Main/Game.tscn")
 		else:
-			get_tree().change_scene("res://src/Main/Splitscreen.tscn")
+			get_tree().change_scene_to_file("res://src/Main/Splitscreen.tscn")
