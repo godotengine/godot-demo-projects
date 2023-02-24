b6b1c1b24b77b20d548b37d6155b2346ac80a7b2
Hugo Locurcio <hugo.locurcio@hugo.pro>
2023-02-24 23:39:33 +0100

    Apply suggestions from code review

--- 2d/navigation/character.gd
+++ 2d/navigation/character.gd
@@ -1,34 +1,33 @@
 extends Sprite
 
-
 export(float) var character_speed = 400.0
 
-onready var navigationagent : NavigationAgent2D = $NavigationAgent2D
-onready var navigationpathdebugline : Line2D = $Line2D
+onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+onready var navigation_path_debug_line: Line2D = $Line2D
 
 
 func _ready():
-	navigationpathdebugline.set_as_toplevel(true)
-	set_physics_process(true)
+	navigation_path_debug_line.set_as_toplevel(true)
 
 
-func set_target_location(target_location : Vector2):
-	navigationagent.set_target_location(target_location)
-	# alternative way to get a full navigation path with Server API
-	var navigation_map : RID = get_world_2d().get_navigation_map()
-	var start_position : Vector2 = get_global_transform().get_origin()
-	var target_position : Vector2 = target_location
-	var optimize : bool = true
-	navigationpathdebugline.points = Navigation2DServer.map_get_path(
-		navigation_map,
-		start_position,
-		target_position,
-		optimize
-		)
+func set_target_location(target_location: Vector2):
+	navigation_agent.set_target_location(target_location)
+	# Alternative way to get a full navigation path with Server API.
+	var navigation_map: RID = get_world_2d().get_navigation_map()
+	var start_position: Vector2 = get_global_transform().get_origin()
+	var target_position: Vector2 = target_location
+	var optimize: bool = true
+	navigation_path_debug_line.points = Navigation2DServer.map_get_path(
+			navigation_map,
+			start_position,
+			target_position,
+			optimize
+	)
 
 
 func _physics_process(delta):
-	if navigationagent.is_navigation_finished():
+	if navigation_agent.is_navigation_finished():
 		return
-	var next_position : Vector2 = navigationagent.get_next_location()
+
+	var next_position: Vector2 = navigation_agent.get_next_location()
 	global_position = global_position.move_toward(next_position, delta * character_speed)
