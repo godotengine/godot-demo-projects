b4252c05bff6a3837b8acd288e648ba35effd475
Danil Alexeev <danil@alexeev.xyz>
2023-03-01 09:46:50 +0300

    Update "Dodge the Creeps" demo

--- 2d/dodge_the_creeps/Player.gd
+++ 2d/dodge_the_creeps/Player.gd
@@ -28,15 +28,14 @@ func _process(delta):
 		$AnimatedSprite2D.stop()
 
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	position = position.clamp(Vector2.ZERO, screen_size)
 
 	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "right"
+		$AnimatedSprite2D.animation = &"right"
 		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.animation = &"up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
 
@@ -48,6 +47,6 @@ func start(pos):
 
 func _on_Player_body_entered(_body):
 	hide() # Player disappears after being hit.
-	emit_signal("hit")
+	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
+	$CollisionShape2D.set_deferred(&"disabled", true)
