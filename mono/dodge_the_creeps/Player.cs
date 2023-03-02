b1aa7feccae161db9a2b1602b006b6c1d1ef2cf2
Ivan Shakhov <van800@gmail.com>
2023-03-02 17:43:14 +0100

    Update mono/dodge_the_creeps/Player.cs
    Co-authored-by: Raul Santos <raulsntos@gmail.com>

--- mono/dodge_the_creeps/Player.cs
+++ mono/dodge_the_creeps/Player.cs
@@ -7,7 +7,7 @@ public partial class Player : Area2D
 
     [Export] public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
 
-    public Vector2 ScreenSize; // Size of the game window.
+    public Vector2 ScreenSize { get; set; } // Size of the game window.
 
     public override void _Ready()
     {
