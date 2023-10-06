5bbda0d1a6e9d98d5dbf47c720c3d7dafb31de16
David Briscoe <idbrii@gmail.com>
2023-10-06 01:13:59 -0700

    Apply type hints to 3d/navigation
    As recommended in #868.

--- 3d/navigation/character.gd
+++ 3d/navigation/character.gd
@@ -3,13 +3,12 @@ extends Marker3D
 
 const Line3D = preload("res://line3d.gd")
 
+@export var character_speed := 10.0
+@export var show_path := true
 
-@export var character_speed: float = 10.0
-@export var show_path = true
+@onready var _nav_agent := $NavigationAgent3D as NavigationAgent3D
 
-@onready var _nav_agent = $NavigationAgent3D
-
-var _nav_path_line
+var _nav_path_line : Line3D
 
 
 func _ready():
@@ -21,8 +20,8 @@ func _ready():
 func _physics_process(delta):
 	if _nav_agent.is_navigation_finished():
 		return
-	var next_position = _nav_agent.get_next_path_position()
-	var offset = next_position - global_position
+	var next_position := _nav_agent.get_next_path_position()
+	var offset := next_position - global_position
 	global_position = global_position.move_toward(next_position, delta * character_speed)
 
 	# Make the robot look at the direction we're traveling.
@@ -35,10 +34,10 @@ func set_target_position(target_position: Vector3):
 	_nav_agent.set_target_position(target_position)
 	# Get a full navigation path with the NavigationServer API.
 	if show_path:
-		var start_position = global_transform.origin
-		var optimize = true
-		var navigation_map = get_world_3d().get_navigation_map()
-		var path = NavigationServer3D.map_get_path(
+		var start_position := global_transform.origin
+		var optimize := true
+		var navigation_map := get_world_3d().get_navigation_map()
+		var path := NavigationServer3D.map_get_path(
 				navigation_map,
 				start_position,
 				target_position,
--- 3d/navigation/navmesh.gd
+++ 3d/navigation/navmesh.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 
-@onready var _camera = $CameraBase/Camera3D as Camera3D
-@onready var _robot = $RobotBase
+const Character = preload("res://character.gd")
+
+@onready var _camera := $CameraBase/Camera3D as Camera3D
+@onready var _robot := $RobotBase as Character
 
 var _cam_rotation := 0.0
 
@@ -11,11 +13,11 @@ func _unhandled_input(event: InputEvent):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		# Get closest point on navmesh for the current mouse cursor position.
 		var mouse_cursor_position = event.position
-		var camera_ray_length = 1000.0
-		var camera_ray_start = _camera.project_ray_origin(mouse_cursor_position)
-		var camera_ray_end = camera_ray_start + _camera.project_ray_normal(mouse_cursor_position) * camera_ray_length
+		var camera_ray_length := 1000.0
+		var camera_ray_start := _camera.project_ray_origin(mouse_cursor_position)
+		var camera_ray_end := camera_ray_start + _camera.project_ray_normal(mouse_cursor_position) * camera_ray_length
 
-		var closest_point_on_navmesh = NavigationServer3D.map_get_closest_point_to_segment(
+		var closest_point_on_navmesh := NavigationServer3D.map_get_closest_point_to_segment(
 			get_world_3d().navigation_map,
 			camera_ray_start,
 			camera_ray_end
