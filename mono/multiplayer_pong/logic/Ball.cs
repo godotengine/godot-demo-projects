278dab56901aef29a6127615771cf36ce40b8574
Allyson <abc9295@nyu.edu>
2024-04-14 16:45:02 -0400

    Updated Multiplayer Pong C# to Godot 4.2

--- mono/multiplayer_pong/logic/Ball.cs
+++ mono/multiplayer_pong/logic/Ball.cs
@@ -3,95 +3,95 @@ using System;
 
 public partial class Ball : Area2D
 {
-    private const int DefaultSpeed = 100;
+	private const int DefaultSpeed = 100;
 
-    private Vector2 _direction = Vector2.Left;
-    private bool _stopped = false;
-    private float _speed = DefaultSpeed;
-    private Vector2 _screenSize;
+	private Vector2 _direction = Vector2.Left;
+	private bool _stopped = false;
+	private double _speed = DefaultSpeed;
+	private Vector2 _screenSize;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        _screenSize = GetViewportRect().Size;
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_screenSize = GetViewportRect().Size;
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        _speed += delta;
-        // Ball will move normally for both players,
-        // even if it's slightly out of sync between them,
-        // so each player sees the motion as smooth and not jerky.
-        if (!_stopped)
-        {
-            Translate(_speed * delta * _direction);
-        }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		_speed += delta;
+		// Ball will move normally for both players,
+		// even if it's slightly out of sync between them,
+		// so each player sees the motion as smooth and not jerky.
+		if (!_stopped)
+		{
+			Translate((float)(_speed * delta) * _direction);
+		}
 
-        // Check screen bounds to make ball bounce.
-        var ballPosition = Position;
-        if ((ballPosition.y < 0 && _direction.y < 0) || (ballPosition.y > _screenSize.y && _direction.y > 0))
-        {
-            _direction.y = -_direction.y;
-        }
+		// Check screen bounds to make ball bounce.
+		var ballPosition = Position;
+		if ((ballPosition.Y < 0 && _direction.Y < 0) || (ballPosition.Y > _screenSize.Y && _direction.Y > 0))
+		{
+			_direction.Y = -_direction.Y;
+		}
 
-        if (IsNetworkMaster())
-        {
-            // Only the master will decide when the ball is out in
-            // the left side (it's own side). This makes the game
-            // playable even if latency is high and ball is going
-            // fast. Otherwise ball might be out in the other
-            // player's screen but not this one.
-            if (ballPosition.x < 0)
-            {
-                GetParent().Rpc("UpdateScore", false);
-                Rpc("ResetBall", false);
-            }
-            else
-            {
-                // Only the puppet will decide when the ball is out in
-                // the right side, which is it's own side. This makes
-                // the game playable even if latency is high and ball
-                // is going fast. Otherwise ball might be out in the
-                // other player's screen but not this one.
-                if (ballPosition.x > _screenSize.x)
-                {
-                    GetParent().Rpc("UpdateScore", true);
-                    Rpc("ResetBall", true);
-                }
-            }
-        }
-    }
+		if (IsMultiplayerAuthority())
+		{
+			// Only the server (Multiplayer Authority) will decide
+			// when the ball is out in the left side (it's own side).
+			// This makes the game playable even if latency is high and
+			// ball is going fast. Otherwise ball might be out in the other
+			// player's screen but not this one.
+			if (ballPosition.X < 0)
+			{
+				GetParent().Rpc("UpdateScore", false);
+				Rpc("ResetBall", false);
+			}
+			else
+			{
+				// Only the peer will decide when the ball is out in
+				// the right side, which is it's own side. This makes
+				// the game playable even if latency is high and ball
+				// is going fast. Otherwise ball might be out in the
+				// other player's screen but not this one.
+				if (ballPosition.X > _screenSize.X)
+				{
+					GetParent().Rpc("UpdateScore", true);
+					Rpc("ResetBall", true);
+				}
+			}
+		}
+	}
 
-    [Sync]
-    private void Bounce(bool left, float random)
-    {
-        // Using sync because both players can make it bounce.
-        if (left)
-        {
-            _direction.x = Mathf.Abs(_direction.x);
-        }
-        else
-        {
-            _direction.x = -Mathf.Abs(_direction.x);
-        }
+	[Rpc(mode: MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
+	private void Bounce(bool left, float random)
+	{
+		// Using sync because both players can make it bounce.
+		if (left)
+		{
+			_direction.X = Mathf.Abs(_direction.X);
+		}
+		else
+		{
+			_direction.X = -Mathf.Abs(_direction.X);
+		}
 
-        _speed *= 1.1f;
-        _direction.y = random * 2.0f - 1;
-        _direction = _direction.Normalized();
-    }
+		_speed *= 1.1f;
+		_direction.Y = random * 2.0f - 1;
+		_direction = _direction.Normalized();
+	}
 
-    [Sync]
-    private void Stop()
-    {
-        _stopped = true;
-    }
+    [Rpc(mode: MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
+	private void Stop()
+	{
+		_stopped = true;
+	}
 
-    [Sync]
-    private void ResetBall(bool forLeft)
-    {
-        Position = _screenSize / 2;
-        _direction = forLeft ? Vector2.Left : Vector2.Right;
-        _speed = DefaultSpeed;
-    }
+    [Rpc(mode: MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
+	private void ResetBall(bool forLeft)
+	{
+		Position = _screenSize / 2;
+		_direction = forLeft ? Vector2.Left : Vector2.Right;
+		_speed = DefaultSpeed;
+	}
 }
--- mono/multiplayer_pong/logic/Lobby.cs
+++ mono/multiplayer_pong/logic/Lobby.cs
@@ -4,140 +4,152 @@ using Godot.Collections;
 
 public partial class Lobby : Control
 {
-    private const int DefaultPort = 8910; // An arbitrary number.
-    private const int MaxNumberOfPeers = 1; // How many people we want to have in a game
+	private const int DefaultPort = 8910; // An arbitrary number.
+	private const int MaxNumberOfPeers = 1; // How many people we want to have in a game
 
-    private LineEdit _address;
-    private Button _hostButton;
-    private Button _joinButton;
-    private Label _statusOk;
-    private Label _statusFail;
-    private NetworkedMultiplayerENet _peer;
+	private LineEdit _address;
+	private Button _hostButton;
+	private Button _joinButton;
+	private Label _statusOk;
+	private Label _statusFail;
+	private ENetMultiplayerPeer _peer;
 
-    public override void _Ready()
-    {
-        // Get nodes - the generic is a class, argument is node path.
-        _address = GetNode<LineEdit>("Address");
-        _hostButton = GetNode<Button>("HostButton");
-        _joinButton = GetNode<Button>("JoinButton");
-        _statusOk = GetNode<Label>("StatusOk");
-        _statusFail = GetNode<Label>("StatusFail");
+	public override void _Ready()
+	{
+		// Get nodes - the generic is a class, argument is node path.
+		_address = GetNode<LineEdit>("Address");
+		_hostButton = GetNode<Button>("HostButton");
+		_joinButton = GetNode<Button>("JoinButton");
+		_statusOk = GetNode<Label>("StatusOk");
+		_statusFail = GetNode<Label>("StatusFail");
 
-        // Connect all callbacks related to networking.
-        // Note: Use snake_case when talking to engine API.
-        GetTree().Connect("network_peer_connected", this, nameof(PlayerConnected));
-        GetTree().Connect("network_peer_disconnected", this, nameof(PlayerDisconnected));
-        GetTree().Connect("connected_to_server", this, nameof(ConnectedOk));
-        GetTree().Connect("connection_failed", this, nameof(ConnectedFail));
-        GetTree().Connect("server_disconnected", this, nameof(ServerDisconnected));
-    }
+		// Connect all callbacks related to networking.
+		// Note: Use snake_case when talking to engine API.
+        // GetTree().GetMultiplayer().PeerConnected += nameof(PlayerConnected());
+        GetTree().GetMultiplayer().Connect("peer_connected", new Callable(this, nameof(PlayerConnected)));
+        GetTree().GetMultiplayer().Connect("peer_disconnected", new Callable(this, nameof(PlayerDisconnected)));
+        GetTree().GetMultiplayer().Connect("connected_to_server", new Callable(this, nameof(ConnectedOk)));
+        GetTree().GetMultiplayer().Connect("connection_failed", new Callable(this, nameof(ConnectedFail)));
+        GetTree().GetMultiplayer().Connect("server_disconnected", new Callable(this, nameof(ServerDisconnected)));
 
-    // Network callbacks from SceneTree
+		// GetTree().Connect("network_peer_connected", this, nameof(PlayerConnected));
+		// GetTree().Connect("network_peer_disconnected", this, nameof(PlayerDisconnected));
+		// GetTree().Connect("connected_to_server", this, nameof(ConnectedOk));
+		// GetTree().Connect("connection_failed", this, nameof(ConnectedFail));
+		// GetTree().Connect("server_disconnected", this, nameof(ServerDisconnected));
+	}
 
-    // Callback from SceneTree.
-    private void PlayerConnected(int id)
-    {
-        // Someone connected, start the game!
-        var pong = ResourceLoader.Load<PackedScene>("res://pong.tscn").Instantiate();
+	// Network callbacks from SceneTree
 
-        // Connect deferred so we can safely erase it from the callback.
-        pong.Connect("GameFinished", this, nameof(EndGame), new Godot.Collections.Array(), (int) ConnectFlags.Deferred);
+	// Callback from SceneTree.
+	private void PlayerConnected(int id)
+	{
+		// Someone connected, start the game!
+		var pong = ResourceLoader.Load<PackedScene>("res://pong.tscn").Instantiate();
 
-        GetTree().Root.AddChild(pong);
-        Hide();
-    }
+		// Connect deferred so we can safely erase it from the callback.
+        pong.Connect("GameFinished", new Callable(this, nameof(EndGame)), (int) ConnectFlags.Deferred);
+        // pong.Connect("GameFinished", this, nameof(EndGame), new Godot.Collections.Array(), (int) ConnectFlags.Deferred);
+        
+		GetTree().Root.AddChild(pong);
+		Hide();
+	}
 
-    private void PlayerDisconnected(int id)
-    {
-        EndGame(GetTree().IsNetworkServer() ? "Client disconnected" : "Server disconnected");
-    }
+	private void PlayerDisconnected(int id)
+	{
+        EndGame(GetTree().GetMultiplayer().IsServer() ? "Client disconnected" : "Server disconnected");
+        
+		// EndGame(GetTree().IsNetworkServer() ? "Client disconnected" : "Server disconnected");
+	}
 
-    // Callback from SceneTree, only for clients (not server).
-    private void ConnectedOk()
-    {
-        // This function is not needed for this project.
-    }
+	// Callback from SceneTree, only for clients (not server).
+	private void ConnectedOk()
+	{
+		// This function is not needed for this project.
+	}
 
-    // Callback from SceneTree, only for clients (not server).
-    private void ConnectedFail()
-    {
-        SetStatus("Couldn't connect", false);
+	// Callback from SceneTree, only for clients (not server).
+	private void ConnectedFail()
+	{
+		SetStatus("Couldn't connect", false);
 
-        GetTree().NetworkPeer = null; // Remove peer.
-        _hostButton.Disabled = false;
-        _joinButton.Disabled = false;
-    }
+		GetTree().GetMultiplayer().MultiplayerPeer = null; // Remove peer.
+		_hostButton.Disabled = false;
+		_joinButton.Disabled = false;
+	}
 
-    private void ServerDisconnected()
-    {
-        EndGame("Server disconnected");
-    }
+	private void ServerDisconnected()
+	{
+		EndGame("Server disconnected");
+	}
 
-    // Game creation functions
+	// Game creation functions
 
-    private void EndGame(string withError = "")
-    {
-        if (HasNode("/root/Pong"))
-        {
-            // Erase immediately, otherwise network might show
-            // errors (this is why we connected deferred above).
-            GetNode("/root/Pong").Free();
-            Show();
-        }
+	private void EndGame(string withError = "")
+	{
+		if (HasNode("/root/Pong"))
+		{
+			// Erase immediately, otherwise network might show
+			// errors (this is why we connected deferred above).
+			GetNode("/root/Pong").Free();
+			Show();
+		}
 
-        GetTree().NetworkPeer = null; // Remove peer.
-        _hostButton.Disabled = false;
-        _joinButton.Disabled = false;
+		GetTree().GetMultiplayer().MultiplayerPeer = null; // Remove peer.
+		_hostButton.Disabled = false;
+		_joinButton.Disabled = false;
 
-        SetStatus(withError, false);
-    }
+		SetStatus(withError, false);
+	}
 
-    private void SetStatus(string text, bool isOk)
-    {
-        // Simple way to show status.
-        if (isOk)
-        {
-            _statusOk.Text = text;
-            _statusFail.Text = "";
-        }
-        else
-        {
-            _statusOk.Text = "";
-            _statusFail.Text = text;
-        }
-    }
+	private void SetStatus(string text, bool isOk)
+	{
+		// Simple way to show status.
+		if (isOk)
+		{
+			_statusOk.Text = text;
+			_statusFail.Text = "";
+		}
+		else
+		{
+			_statusOk.Text = "";
+			_statusFail.Text = text;
+		}
+	}
 
-    private void OnHostPressed()
-    {
-        _peer = new NetworkedMultiplayerENet();
-        _peer.CompressionMode = NetworkedMultiplayerENet.CompressionModeEnum.RangeCoder;
-        Error err = _peer.CreateServer(DefaultPort, MaxNumberOfPeers);
-        if (err != Error.Ok)
-        {
-            // Is another server running?
-            SetStatus("Can't host, address in use.", false);
-            return;
-        }
+	private void OnHostPressed()
+	{
+		_peer = new ENetMultiplayerPeer();
+		Error err = _peer.CreateServer(DefaultPort, MaxNumberOfPeers);
+		if (err != Error.Ok)
+		{
+			// Is another server running?
+			SetStatus("Can't host, address in use.", false);
+			return;
+		}
 
-        GetTree().NetworkPeer = _peer;
-        _hostButton.Disabled = true;
-        _joinButton.Disabled = true;
-        SetStatus("Waiting for player...", true);
-    }
+        _peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
 
-    private void OnJoinPressed()
-    {
-        string ip = _address.Text;
-        if (!ip.IsValidIPAddress())
-        {
-            SetStatus("IP address is invalid", false);
-            return;
-        }
+		GetTree().GetMultiplayer().MultiplayerPeer = _peer;
+		_hostButton.Disabled = true;
+		_joinButton.Disabled = true;
+		SetStatus("Waiting for player...", true);
+	}
 
-        _peer = new NetworkedMultiplayerENet();
-        _peer.CompressionMode = NetworkedMultiplayerENet.CompressionModeEnum.RangeCoder;
-        _peer.CreateClient(ip, DefaultPort);
-        GetTree().NetworkPeer = _peer;
-        SetStatus("Connecting...", true);
-    }
+	private void OnJoinPressed()
+	{
+		string ip = _address.Text;
+		if (!ip.IsValidIPAddress())
+		{
+			SetStatus("IP address is invalid", false);
+			return;
+		}
+
+		_peer = new ENetMultiplayerPeer();
+		_peer.CreateClient(ip, DefaultPort);
+        _peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
+
+		GetTree().GetMultiplayer().MultiplayerPeer = _peer;
+		SetStatus("Connecting...", true);
+	}
 }
