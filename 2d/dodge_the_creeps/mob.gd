5557b10cfa514101fb168c8ff2239594d7d34cff
utsav132 <155137203+utsav132@users.noreply.github.com>
2025-03-08 00:13:58 +0530

    Fix mob playback animation being started before the animation is set in Dodge the Creeps (#1175)
    The '$AnimatedSprite2D.play()' is called at the beginning in the '_ready' function,
    when it should actually be called at the end, after selecting the animation.

--- 2d/dodge_the_creeps/mob.gd
+++ 2d/dodge_the_creeps/mob.gd
@@ -1,9 +1,9 @@
 extends RigidBody2D
 
 func _ready():
-	$AnimatedSprite2D.play()
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
+	$AnimatedSprite2D.play()
 
 
 func _on_VisibilityNotifier2D_screen_exited():
