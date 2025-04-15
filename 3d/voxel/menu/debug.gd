4ef608dd838e0a114225d27b5eb5bd8842c76a85
Hugo Locurcio <hugo.locurcio@hugo.pro>
2025-04-15 15:38:10 +0200

    Add block placement preview to Voxel demo, improve debug display
    - Allow placing/breaking blocks when further away (up to 5 blocks instead of 4).
    - Add a background to the debug display to improve readability on bright backgrounds.
    - Simplify coordinate display using the `%v` placeholder available in 4.3 onwards.

--- 3d/voxel/menu/debug.gd
+++ 3d/voxel/menu/debug.gd
@@ -8,31 +8,11 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed(&"debug"):
 		visible = not visible
 
-	text = "Position: " + _vector_to_string_appropriate_digits(player.transform.origin)
-	text += "\nEffective render distance: " + str(voxel_world.effective_render_distance)
-	text += "\nLooking: " + _cardinal_string_from_radians(player.transform.basis.get_euler().y)
-	text += "\nMemory: " + "%3.0f" % (OS.get_static_memory_usage() / 1048576.0) + " MiB"
-	text += "\nFPS: " + String.num_uint64(Engine.get_frames_per_second())
-
-
-# Avoids the problem of showing more digits than needed or available.
-func _vector_to_string_appropriate_digits(vector: Vector3) -> String:
-	var factors: Array[int] = [1000, 1000, 1000]
-	for i in 3:
-		if abs(vector[i]) > 4096:
-			@warning_ignore("integer_division")
-			factors[i] = factors[i] / 10
-			if abs(vector[i]) > 65536:
-				@warning_ignore("integer_division")
-				factors[i] = factors[i] / 10
-				if abs(vector[i]) > 524288:
-					@warning_ignore("integer_division")
-					factors[i] = factors[i] / 10
-
-	return "(" + \
-			str(round(vector.x * factors[0]) / factors[0]) + ", " + \
-			str(round(vector.y * factors[1]) / factors[1]) + ", " + \
-			str(round(vector.z * factors[2]) / factors[2]) + ")"
+	text = "Position: %.1v" % player.transform.origin \
+			+ "\nEffective render distance: " + str(voxel_world.effective_render_distance) \
+			+ "\nLooking: " + _cardinal_string_from_radians(player.transform.basis.get_euler().y) \
+			+ "\nMemory: " + "%3.0f" % (OS.get_static_memory_usage() / 1048576.0) + " MiB" \
+			+ "\nFPS: %d" % Engine.get_frames_per_second()
 
 
 # Expects a rotation where 0 is North, on the range -PI to PI.
