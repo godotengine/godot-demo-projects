aee8ca4ab9e7544cc773a1a606c832278c6fa217
VargaDot <vancouver_cliparf@simplelogin.com>
2025-05-05 01:26:57 +0100

    Added audio effects demo
    I used dkleitsas' PR with some changes:
    - Swapped all sfx with CC0 licensed ones.
    - get_node() --> $PathtoAudiostream
    - Screenshot converted to webp (52% decrease in size).
    - Swapped background.png for a color rect.
    - Music converted from mp3 to ogg (4.9 MBs to 1.59 MBs)
    - All SFXs are in .wav
    
    Co-Authored-By: dkleitsas <86194775+dkleitsas@users.noreply.github.com>

---
+++ audio/audio_effects/README.md
@@ -0,0 +1,32 @@
+# Audio Effects demo
+
+This is a simple demo that showcases the various Audio Effects that can be used in the Godot engine.
+One or more Audio Effects can be toggled and applied to the playable sound effects and background music.
+
+Language: GDScript
+
+Renderer: Compatibility
+
+## Screenshots
+
+![ScreenShot](screenshots/screenshot1.webp)
+
+## Licenses
+
+### Music
+
+Monkeys Spinning Monkeys Kevin MacLeod (incompetech.com)
+Licensed under Creative Commons: By Attribution 3.0 License
+http://creativecommons.org/licenses/by/3.0/
+
+### Sound Effects
+
+All sound effects are from [Freesound](https://freesound.org/) and licensed under [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
+
+- [Ding](https://freesound.org/people/MatthewWong/sounds/361564/) by MatthewWong
+- [Glass Breaking](https://freesound.org/people/chewiesmissus/sounds/244238/) by chewiesmissus
+- [Meow](https://freesound.org/people/tuberatanka/sounds/110011/) by tuberatanka
+- [Whistle](https://freesound.org/people/OwlStorm/sounds/320150/) by OwlStorm
+- [Negative Beeps](https://freesound.org/people/themusicalnomad/sounds/253886/) by themusicalnomad
+- [Sad Trombone](https://freesound.org/people/kirbydx/sounds/175409/) by kirbydx
+- [Static](https://freesound.org/people/dotY21/sounds/335203/) by dotY21
---
+++ audio/audio_effects/audio_effects.gd
@@ -0,0 +1,136 @@
+extends Node2D
+
+func _on_toggle_music_toggled(button_pressed):
+	if button_pressed:
+		$SoundEffects/Music.play()
+	else:
+		$SoundEffects/Music.stop()
+
+func _on_ding_button_pressed():
+	$SoundEffects/Ding.play()
+
+func _on_glass_button_pressed():
+	$SoundEffects/Glass.play()
+
+func _on_meow_button_pressed():
+	$SoundEffects/Meow.play()
+
+func _on_beeps_button_pressed():
+	$SoundEffects/Beeps.play()
+
+func _on_trombone_button_pressed():
+	$SoundEffects/Trombone.play()
+
+func _on_static_button_pressed():
+	$SoundEffects/Static.play()
+
+func _on_whistle_button_pressed():
+	$SoundEffects/Whistle.play()
+
+func _on_toggle_amplify_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 0, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 0, false)
+
+func _on_toggle_band_limiter_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 1, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 1, false)
+
+func _on_toggle_band_pass_filter_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 2, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 2, false)
+
+func _on_toggle_chorus_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 3, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 3, false)
+
+func _on_toggle_compressor_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 4, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 4, false)
+
+func _on_toggle_delay_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 5, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 5, false)
+
+func _on_toggle_distortion_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 6, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 6, false)
+
+func _on_toggle_eq_6_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 7, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 7, false)
+
+func _on_toggle_eq_10_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 8, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 8, false)
+
+func _on_toggle_eq_21_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 9, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 9, false)
+
+func _on_toggle_high_pass_filter_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 10, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 10, false)
+
+func _on_toggle_low_shelf_filter_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 11, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 11, false)
+
+func _on_toggle_notch_filter_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 12, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 12, false)
+
+func _on_toggle_panner_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 13, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 13, false)
+
+func _on_toggle_phaser_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 14, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 14, false)
+
+func _on_toggle_pitch_shift_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 15, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 15, false)
+
+func _on_toggle_reverb_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 16, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 16, false)
+
+func _on_toggle_stereo_enhance_toggled(button_pressed):
+	if button_pressed:
+		AudioServer.set_bus_effect_enabled(0, 17, true)
+	else:
+		AudioServer.set_bus_effect_enabled(0, 17, false)
