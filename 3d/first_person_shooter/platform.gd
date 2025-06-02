93212837153996ec2eab2d925ae1c721de099d69
Hugo Locurcio <hugo.locurcio@hugo.pro>
2025-06-02 17:44:51 +0200

    Update for 4.5.dev4

--- 3d/first_person_shooter/platform.gd
+++ 3d/first_person_shooter/platform.gd
@@ -15,7 +15,7 @@ var player_on_platform := false
 
 @onready var initial_height := position.y
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	position.y = clamp(position.y + velocity * delta, initial_height, initial_height + distance)
 
 	if is_equal_approx(position.y, initial_height):
@@ -37,7 +37,7 @@ func _on_move_trigger_body_entered(body: Node3D) -> void:
 			velocity = speed
 
 
-func _on_move_trigger_body_exited(body: Node3D) -> void:
+func _on_move_trigger_body_exited(_body: Node3D) -> void:
 	player_on_platform = false
 	if is_zero_approx(velocity) and is_zero_approx($Timer.get_time_left()):
 		# If the timer already expired, start going downwards immediately.
@@ -50,4 +50,3 @@ func _on_timer_timeout() -> void:
 	# as long as they don't leave it.
 	if not player_on_platform:
 		velocity = -speed
-
