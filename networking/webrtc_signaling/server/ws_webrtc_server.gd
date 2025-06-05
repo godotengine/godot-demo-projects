8855d28e7178749606a747e708ad0905c3891e7a
Switchbreak <switch@switchbreak.net>
2025-06-05 16:32:40 -0700

    Fix type check to mirror behavior of JSON.parse_string
    JSON.parse_string will always return float for numeric values

--- networking/webrtc_signaling/server/ws_webrtc_server.gd
+++ networking/webrtc_signaling/server/ws_webrtc_server.gd
@@ -207,7 +207,7 @@ func _parse_msg(peer: Peer) -> bool:
 	if typeof(parsed) != TYPE_DICTIONARY or not parsed.has("type") or not parsed.has("id") or \
 		typeof(parsed.get("data")) != TYPE_STRING:
 		return false
-	if not str(parsed.type).is_valid_int() or not str(parsed.id).is_valid_int():
+	if not typeof(parsed.type) == Variant.Type.TYPE_FLOAT or not typeof(parsed.id) == Variant.Type.TYPE_FLOAT:
 		return false
 
 	var msg := {
