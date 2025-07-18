6c39a8fe1f6226acbf1eb94fb7c55f9934414f5f
Calinou <hugo.locurcio@hugo.pro>
2025-07-18 14:43:36 +0000

    Deploying to gh-pages from @ godotengine/godot-demo-projects@ba8718ed097928fa742c7c6a6b495bc86f58a877 🚀

---
+++ viewport/2d_in_3d/index.worker.js
@@ -0,0 +1,5 @@
+// This file is no longer used by emscripten and has been created as a placeholder
+// to allow build systems to transition away from depending on it.
+//
+// Future versions of emscripten will likely stop generating this file at all.
+throw new Error('Dummy worker.js file should never be used');
