11281178c5f197c6819b0efdff82dace237bb12c
KOGA Mitsuhiro <shiena.jp@gmail.com>
2025-08-25 18:34:25 +0900

    Address review comments

--- misc/camera_feed/camerafeed.gd
+++ misc/camera_feed/camerafeed.gd
@@ -11,24 +11,26 @@ var camera_feed: CameraFeed
 
 const defaultWebResolution: Dictionary = {
 	"width": 640,
-	"height": 480
+	"height": 480,
 }
 
 func _ready() -> void:
 	_adjust_ui()
 	_reload_camera_list()
 
+
 func _adjust_ui() -> void:
 	camera_display.size = camera_display.get_parent_area_size() - Vector2.ONE * 40
 	camera_preview.custom_minimum_size = camera_display.size
 	camera_preview.position = camera_display.size / 2
+
 
 func _reload_camera_list() -> void:
 	camera_list.clear()
 	format_list.clear()
 
 	var os_name := OS.get_name()
-	# Request camera permission on mobile
+	# Request camera permission on mobile.
 	if os_name in ["Android", "iOS"]:
 		var permissions = OS.get_granted_permissions()
 		if not "CAMERA" in permissions:
@@ -38,13 +40,16 @@ func _reload_camera_list() -> void:
 
 	if not CameraServer.camera_feeds_updated.is_connected(_on_camera_feeds_updated):
 		CameraServer.camera_feeds_updated.connect(_on_camera_feeds_updated)
+
 	if CameraServer.monitoring_feeds:
 		CameraServer.monitoring_feeds = false
 		await get_tree().process_frame
+
 	CameraServer.monitoring_feeds = true
 
+
 func _on_camera_feeds_updated() -> void:
-	# Get available camera feeds
+	# Get available camera feeds.
 	var feeds = CameraServer.feeds()
 	if feeds.is_empty():
 		camera_list.add_item("No cameras found")
@@ -59,24 +64,26 @@ func _on_camera_feeds_updated() -> void:
 		var feed: CameraFeed = feeds[i]
 		camera_list.add_item(feed.get_name())
 
-	# Auto-select first camera
+	# Auto-select first camera.
 	camera_list.selected = 0
 	_on_camera_list_item_selected(0)
+
 
 func _on_camera_list_item_selected(index: int) -> void:
 	var camera_feeds := CameraServer.feeds()
 	if index < 0 or index >= camera_feeds.size():
 		return
 
-	# Stop previous camera if active
+	# Stop previous camera if active.
 	if camera_feed and camera_feed.feed_is_active:
 		camera_feed.feed_is_active = false
 
-	# Get selected camera feed
+	# Get selected camera feed.
 	camera_feed = camera_feeds[index]
 
-	# Update format list
+	# Update format list.
 	_update_format_list()
+
 
 func _update_format_list() -> void:
 	format_list.clear()
@@ -99,9 +106,10 @@ func _update_format_list() -> void:
 			item += " : %s / %s" % [format["frame_denominator"], format["frame_numerator"]]
 		format_list.add_item(item)
 
-	# Auto-select first format
+	# Auto-select first format.
 	format_list.selected = 0
 	_on_format_list_item_selected(0)
+
 
 func _on_format_list_item_selected(index: int) -> void:
 	if not camera_feed:
@@ -115,27 +123,32 @@ func _on_format_list_item_selected(index: int) -> void:
 	camera_feed.set_format(index, parameters)
 	_start_camera_feed()
 
+
 func _start_camera_feed() -> void:
 	if not camera_feed:
 		return
 
 	if not camera_feed.frame_changed.is_connected(_on_frame_changed):
 		camera_feed.frame_changed.connect(_on_frame_changed, ConnectFlags.CONNECT_ONE_SHOT | ConnectFlags.CONNECT_DEFERRED)
-	# Start the feed
+	# Start the feed.
 	camera_feed.feed_is_active = true
+
 
 func _on_frame_changed() -> void:
 	var datatype := camera_feed.get_datatype() as CameraFeed.FeedDataType
 	var preview_size := Vector2.ZERO
+
 	var mat: ShaderMaterial = camera_preview.material
 	var rgb_texture: CameraTexture = mat.get_shader_parameter("rgb_texture")
 	var y_texture: CameraTexture = mat.get_shader_parameter("y_texture")
 	var cbcr_texture: CameraTexture = mat.get_shader_parameter("cbcr_texture")
 	var ycbcr_texture: CameraTexture = mat.get_shader_parameter("ycbcr_texture")
+
 	rgb_texture.which_feed = CameraServer.FeedImage.FEED_RGBA_IMAGE
 	y_texture.which_feed = CameraServer.FeedImage.FEED_Y_IMAGE
 	cbcr_texture.which_feed = CameraServer.FeedImage.FEED_CBCR_IMAGE
 	ycbcr_texture.which_feed = CameraServer.FEED_YCBCR_IMAGE
+
 	match datatype:
 		CameraFeed.FeedDataType.FEED_RGB:
 			rgb_texture.camera_feed_id = camera_feed.get_id()
@@ -155,20 +168,25 @@ func _on_frame_changed() -> void:
 			mat.set_shader_parameter("mode", 2)
 			preview_size = ycbcr_texture.get_size()
 		_:
-			print("YCbCr format not fully implemented yet")
+			print("Skip formats that are not supported.")
 			return
+
 	var white_image := Image.create(int(preview_size.x), int(preview_size.y), false, Image.FORMAT_RGBA8)
 	white_image.fill(Color.WHITE)
 	camera_preview.texture = ImageTexture.create_from_image(white_image)
+
 	var rot := camera_feed.feed_transform.get_rotation()
 	var degree := roundi(rad_to_deg(rot))
 	camera_preview.rotation = rot
 	camera_preview.custom_minimum_size.y = camera_display.size.y
+
 	if degree % 180 == 0:
 		camera_display.ratio = preview_size.x / preview_size.y
 	else:
 		camera_display.ratio = preview_size.y / preview_size.x
+
 	start_or_stop_button.text = "Stop"
+
 
 func _on_start_or_stop_button_pressed(change_label: bool = true) -> void:
 	if camera_feed and camera_feed.feed_is_active:
@@ -181,6 +199,7 @@ func _on_start_or_stop_button_pressed(change_label: bool = true) -> void:
 		_start_camera_feed()
 		if change_label:
 			start_or_stop_button.text = "Stop"
+
 
 func _on_reload_button_pressed() -> void:
 	_on_start_or_stop_button_pressed(false)
