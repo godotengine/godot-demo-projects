3b7ab7d9f7e99a49582e9804ad25ade799b53036
David Briscoe <idbrii@gmail.com>
2025-08-24 20:09:56 -0700

    Prevent free movement troll from leaving tile area
    Move troll's root pivot to line up with their collider. This makes their
    global_position a good representation of where they are.
    
    Add check for moving to an invalid tile and undo our move. In my tests,
    this isn't less smooth than skipping the move_and_slide if
    (global_position + velocity * dt) is an invalid tile and allows sliding
    to resolve us to a slightly different position.

--- 2d/hexagonal_map/troll_free.gd
+++ 2d/hexagonal_map/troll_free.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-const MOTION_SPEED = 30
-const FRICTION_FACTOR = 0.89
-const TAN30DEG = tan(deg_to_rad(30))
+const MOTION_SPEED: float = 30.0
+const FRICTION_FACTOR: float = 0.89
+const TAN30DEG: float = tan(deg_to_rad(30.0))
+
+
+@export var grid: TileMapLayer
 
 
 func become_active_troll() -> void:
@@ -10,6 +13,8 @@ func become_active_troll() -> void:
 
 
 func _physics_process(_delta: float) -> void:
+	var prev_position: Vector2 = global_position
+
 	var motion := Vector2()
 	motion.x = Input.get_axis(&"move_left", &"move_right")
 	motion.y = Input.get_axis(&"move_up", &"move_down")
@@ -19,3 +24,12 @@ func _physics_process(_delta: float) -> void:
 	# Apply friction.
 	velocity *= FRICTION_FACTOR
 	move_and_slide()
+
+
+	# Prevent movement off of the grid tiles. Alternatively, you could add a
+	# border tile around the outside of your world with physics enabled in the
+	# TileMapLayer. That would allow nice sliding along the walls of the world.
+	var dest_tile: int = grid.get_world_tile(global_position)
+	var has_tile: bool = dest_tile >= 0
+	if not has_tile:
+		global_position = prev_position
