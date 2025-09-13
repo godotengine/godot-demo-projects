5088f4b051f3a0f11f5ed94dc6cc2f76b8ff44cf
David Briscoe <idbrii@gmail.com>
2025-09-13 08:35:34 -0700

    Use SpriteFrames instead of animating texture
    Fix #1114: Some character animations are not displayed in JRPG Demo.
    
    Seems like Godot 4 doesn't allow toggling AnimationPlayer tracks by
    making children editable so the texture is no longer being set and the
    player is invisible. Regardless, this workflow is also not scalable to
    multiple enemy types. Instead, use SpriteFrames to setup different
    visuals for each slime.
    
    Triggers animations from code alongside AnimationPlayer calls because
    doing it in AnimationPlayer seemed unnecessarily complex.
    
    Test
    * Walk around and bump into walls and opponent. Player is yellow and
      plays two animations. Opponent is blue. Combat triggers on contact.

--- 2d/role_playing_game/grid_movement/pawns/opponent.gd
+++ 2d/role_playing_game/grid_movement/pawns/opponent.gd
@@ -1,7 +1,12 @@
 extends Pawn
 
 @export var combat_actor: PackedScene
+@export var pose_anims: SpriteFrames
+
 var lost := false
 
+@onready var pose := $Pivot/Slime
+
 func _ready() -> void:
+	pose.sprite_frames = pose_anims
 	set_process(false)
--- 2d/role_playing_game/grid_movement/pawns/walker.gd
+++ 2d/role_playing_game/grid_movement/pawns/walker.gd
@@ -1,6 +1,7 @@
 extends Pawn
 
 @export var combat_actor: PackedScene
+@export var pose_anims: SpriteFrames
 
 var lost := false
 var grid_size: float
@@ -8,9 +9,11 @@ var grid_size: float
 @onready var parent := get_parent()
 @onready var animation_playback: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
 @onready var walk_animation_time: float = $AnimationPlayer.get_animation("walk").length
+@onready var pose := $Pivot/Slime
 
 
 func _ready() -> void:
+	pose.sprite_frames = pose_anims
 	update_look_direction(Vector2.RIGHT)
 	grid_size = parent.tile_set.tile_size.x
 
@@ -43,6 +46,7 @@ func update_look_direction(direction: Vector2) -> void:
 func move_to(target_position: Vector2) -> void:
 	set_process(false)
 	var move_direction := (target_position - position).normalized()
+	pose.play("idle")
 	animation_playback.start("walk")
 
 	var tween := create_tween()
@@ -54,13 +58,16 @@ func move_to(target_position: Vector2) -> void:
 	$Pivot.position = Vector2.ZERO
 	position = target_position
 	animation_playback.start("idle")
+	pose.play("idle")
 
 	set_process(true)
 
 
 func bump() -> void:
 	set_process(false)
+	pose.play("bump")
 	animation_playback.start("bump")
 	await $AnimationTree.animation_finished
 	animation_playback.start("idle")
+	pose.play("idle")
 	set_process(true)
