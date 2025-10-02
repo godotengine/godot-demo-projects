f6d0de200727f51faf1e8447fae6854e306edcf2
Aaron Franke <arnfranke@yahoo.com>
2025-10-01 22:36:55 -0700

    Style fixes, layout improvements, update to Godot 4.5

--- viewport/split_screen_input/split_screen.gd
+++ viewport/split_screen_input/split_screen.gd
@@ -1,40 +1,40 @@
+## Interface for a SplitScreen
 class_name SplitScreen
 extends Node
-## Interface for a SplitScreen
 
 
-const keypad_string:String = "Joypad" # Prefix for joypads.
+const JOYPAD_PREFIX: String = "Joypad"
 
-@export var init_position: Vector2
+@export var init_position := Vector2.ZERO
 
 var _keyboard_options: Dictionary # Copy of all keyboard options.
 
 @onready var opt: OptionButton = $OptionButton
-@onready var v: SubViewport = $SubViewportContainer/SubViewport
-@onready var svc: MySV = $SubViewportContainer
-@onready var play: Player = $SubViewportContainer/SubViewport/Player
+@onready var viewport: SubViewport = $InputRoutingViewportContainer/SubViewport
+@onready var input_router: InputRoutingViewportContainer = $InputRoutingViewportContainer
+@onready var play: Player = $InputRoutingViewportContainer/SubViewport/Player
 
 
 # Set the configuration of this split screen and perform OptionButton initialization.
-func set_config(c: Dictionary):
-	_keyboard_options = c["keyboard"]
-	play.position = c["position"]
-	var local_index = c["index"]
-	play.modulate = c["color"]
+func set_config(config_dict: Dictionary):
+	_keyboard_options = config_dict["keyboard"]
+	play.position = config_dict["position"]
+	var local_index: int = config_dict["index"]
+	play.modulate = config_dict["color"]
 	opt.clear()
-	for k in _keyboard_options:
-		opt.add_item(k)
-	for i in c["joypads"]:
-		opt.add_item("%s %s" % [keypad_string, i+1])
+	for keyboard_opt in _keyboard_options:
+		opt.add_item(keyboard_opt)
+	for index in config_dict["joypads"]:
+		opt.add_item("%s %s" % [JOYPAD_PREFIX, index + 1])
 	opt.select(local_index)
 	_on_option_button_item_selected(local_index)
-	v.world_2d = c["world"] # Connect all Split Screens to the same World2D.
+	viewport.world_2d = config_dict["world"] # Connect all Split Screens to the same World2D.
 
 
 # Update Keyboard Settings after selecting them in the OptionButton.
 func _on_option_button_item_selected(index: int) -> void:
-	var txt: String = opt.get_item_text(index)
-	if txt.begins_with(keypad_string):
-		svc.set_input_config({"joypad": txt.substr(txt.length()-1, -1).to_int(), "keyboard": []})
+	var text: String = opt.get_item_text(index)
+	if text.begins_with(JOYPAD_PREFIX):
+		input_router.set_input_config({"joypad": text.substr(text.length() - 1, -1).to_int(), "keyboard": []})
 	else:
-		svc.set_input_config({"keyboard": _keyboard_options[txt]["keys"], "joypad": -1})
+		input_router.set_input_config({"keyboard": _keyboard_options[text]["keys"], "joypad": -1})
--- viewport/split_screen_input/sub_viewport_container.gd
+++ viewport/split_screen_input/sub_viewport_container.gd
@@ -1,9 +1,8 @@
-class_name MySV
-extends SubViewportContainer
 ## Input Routing for different SubViewports.
-#
 ## Based on the provided input configuration, ensures only the correct
-## events reaching the SubViewport-
+## events reaching the SubViewport.
+class_name InputRoutingViewportContainer
+extends SubViewportContainer
 
 
 var _current_keyboard_set: Array = [] # Currently used keyboard set.
@@ -12,17 +11,17 @@ var _current_joypad_device: int = -1 # Currently used joypad device id.
 
 # Make sure, that only the events are sent to the SubViewport,
 # that are allowed via the OptionButton selection.
-func _propagate_input_event(event: InputEvent) -> bool:
-	if event is InputEventKey:
-		if _current_keyboard_set.has(event.keycode):
+func _propagate_input_event(input_event: InputEvent) -> bool:
+	if input_event is InputEventKey:
+		if _current_keyboard_set.has(input_event.keycode):
 			return true
-	elif event is InputEventJoypadButton:
-		if _current_joypad_device > -1 and event.device == _current_joypad_device:
+	elif input_event is InputEventJoypadButton:
+		if _current_joypad_device > -1 and input_event.device == _current_joypad_device:
 			return true
 	return false
 
 
 # Set new config for input handling.
-func set_input_config(config: Dictionary):
-	_current_keyboard_set = config["keyboard"]
-	_current_joypad_device = config["joypad"]
+func set_input_config(config_dict: Dictionary):
+	_current_keyboard_set = config_dict["keyboard"]
+	_current_joypad_device = config_dict["joypad"]
