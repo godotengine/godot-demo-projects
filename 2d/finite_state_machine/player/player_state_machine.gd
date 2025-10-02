1d8fa9c44dbc8a441e6c427c4e655aa9b2fa6d5d
Yaxian <yaxian.gu@gmail.com>
2025-10-02 12:33:27 +0800

    Add player_state dictionary to manage player state names for FSM demo (#1143)
    * Add player_state dictionary to manage player state names for FSM demo
    
    * Use a constant typed Dictionary of StringNames and update to Godot 4.5
    
    ---------
    
    Co-authored-by: Aaron Franke <arnfranke@yahoo.com>

--- 2d/finite_state_machine/player/player_state_machine.gd
+++ 2d/finite_state_machine/player/player_state_machine.gd
@@ -1,5 +1,7 @@
 extends "res://state_machine/state_machine.gd"
 
+var PLAYER_STATE = preload("res://player/player_state.gd").PLAYER_STATE
+
 @onready var idle: Node = $Idle
 @onready var move: Node = $Move
 @onready var jump: Node = $Jump
@@ -8,11 +10,11 @@ extends "res://state_machine/state_machine.gd"
 
 func _ready() -> void:
 	states_map = {
-		"idle": idle,
-		"move": move,
-		"jump": jump,
-		"stagger": stagger,
-		"attack": attack,
+		PLAYER_STATE.idle: idle,
+		PLAYER_STATE.move: move,
+		PLAYER_STATE.jump: jump,
+		PLAYER_STATE.stagger: stagger,
+		PLAYER_STATE.attack: attack,
 	}
 
 
@@ -20,21 +22,21 @@ func _change_state(state_name: String) -> void:
 	# The base state_machine interface this node extends does most of the work.
 	if not _active:
 		return
-	if state_name in ["stagger", "jump", "attack"]:
+	if state_name in [PLAYER_STATE.stagger, PLAYER_STATE.jump, PLAYER_STATE.attack]:
 		states_stack.push_front(states_map[state_name])
-	if state_name == "jump" and current_state == move:
+	if state_name == PLAYER_STATE.jump and current_state == move:
 		jump.initialize(move.speed, move.velocity)
 
 	super._change_state(state_name)
 
 
-func _unhandled_input(event: InputEvent) -> void:
+func _unhandled_input(input_event: InputEvent) -> void:
 	# Here we only handle input that can interrupt states, attacking in this case,
 	# otherwise we let the state node handle it.
-	if event.is_action_pressed("attack"):
+	if input_event.is_action_pressed(PLAYER_STATE.attack):
 		if current_state in [attack, stagger]:
 			return
-		_change_state("attack")
+		_change_state(PLAYER_STATE.attack)
 		return
 
-	current_state.handle_input(event)
+	current_state.handle_input(input_event)
--- 2d/finite_state_machine/player/states/combat/attack.gd
+++ 2d/finite_state_machine/player/states/combat/attack.gd
@@ -1,8 +1,8 @@
-extends "res://state_machine/state.gd"
+extends "res://player/player_state.gd"
 
 func enter() -> void:
-	owner.get_node(^"AnimationPlayer").play("idle")
+	owner.get_node(^"AnimationPlayer").play(PLAYER_STATE.idle)
 
 
 func _on_Sword_attack_finished() -> void:
-	finished.emit("previous")
+	finished.emit(PLAYER_STATE.previous)
