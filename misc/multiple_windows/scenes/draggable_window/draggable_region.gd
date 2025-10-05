dd91bd53dfe83151cea8135f97b2350f5ea29d30
Vissa <46628714+Visssarion@users.noreply.github.com>
2025-10-06 01:55:56 +0300

    Replace drag implementation

--- misc/multiple_windows/scenes/draggable_window/draggable_region.gd
+++ misc/multiple_windows/scenes/draggable_window/draggable_region.gd
@@ -1,19 +1,6 @@
 extends Area2D
 
-var window_delta: Vector2i
-
-var held_down: bool = false
-
 func _input_event(_viewport: Viewport, event: InputEvent, _shape_index: int) -> void:
 	if event is InputEventMouseButton:
-		held_down = event.pressed
 		if event.pressed:
-			window_delta = get_window().position - DisplayServer.mouse_get_position()
-
-
-func _process(_delta: float) -> void:
-	if held_down:
-		get_window().position = DisplayServer.mouse_get_position() + window_delta
-		var mouse_state = DisplayServer.mouse_get_button_state()
-		if mouse_state & MOUSE_BUTTON_MASK_LEFT == 0:
-			held_down = false
+			get_window().start_drag()
