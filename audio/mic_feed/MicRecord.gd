5405b6493a0c183602f87a091e1614ce32792110
Julian Todd <julian@goatchurch.org.uk>
2025-10-13 16:43:05 +0100

    Apply suggestions from code review
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>

--- audio/mic_feed/MicRecord.gd
+++ audio/mic_feed/MicRecord.gd
@@ -29,7 +29,7 @@ func _ready() -> void:
 	input_mix_rate = int(AudioServer.get_input_mix_rate())
 	print("Input mix rate: ", input_mix_rate)
 	print("Output mix rate: ", AudioServer.get_mix_rate())
-	print("Project mix rate: ", ProjectSettings.get("audio/driver/mix_rate"))
+	print("Project mix rate: ", ProjectSettings.get(&"audio/driver/mix_rate"))
 
 	if Engine.has_singleton("MicrophoneServer"):
 		microphonefeed = Engine.get_singleton("MicrophoneServer").get_feed(0)
@@ -40,22 +40,26 @@ func _ready() -> void:
 		$MicrophoneOn.disabled = true
 
 	$InputMixRate.text = "Mix rate: %d" % input_mix_rate
-	audio_sample_size = int(audio_chunk_size_ms*input_mix_rate/1000.0)
+	audio_sample_size = int(audio_chunk_size_ms * input_mix_rate / 1000.0)
+
 	var blank_image: PackedVector2Array = PackedVector2Array()
 	blank_image.resize(audio_sample_size)
 	audio_sample_image = Image.create_from_data(audio_sample_size, 1, false, Image.FORMAT_RGF, blank_image.to_byte_array())
 	audio_sample_texture = ImageTexture.create_from_image(audio_sample_image)
 	$MicTexture.material.set_shader_parameter(&"audiosample", audio_sample_texture)
 
+
 func _on_option_input_item_selected(index: int) -> void:
-	var inputdevice: String = $OptionInput.get_item_text(index)
+	var input_device: String = $OptionInput.get_item_text(index)
 	print("Set input device: ", inputdevice)
 	AudioServer.set_input_device(inputdevice)
 
+
 func _on_option_output_item_selected(index: int) -> void:
-	var outputdevice: String = $OptionOutput.get_item_text(index)
+	var output_device: String = $OptionOutput.get_item_text(index)
 	print("Set output device: ", outputdevice)
 	AudioServer.set_output_device(outputdevice)
+
 
 func _on_microphone_on_toggled(toggled_on: bool) -> void:
 	if toggled_on:
@@ -66,7 +70,7 @@ func _on_microphone_on_toggled(toggled_on: bool) -> void:
 			var x = await get_tree().on_request_permissions_result
 			var permission: String = x[0]
 			var granted: bool = x[1]
-			assert (permission == "android.permission.RECORD_AUDIO")
+			assert(permission == "android.permission.RECORD_AUDIO")
 			print("Audio permission granted ", granted)
 
 		if not microphonefeed.is_active():
@@ -74,14 +78,16 @@ func _on_microphone_on_toggled(toggled_on: bool) -> void:
 		total_samples = 0
 		sample_duration = 0.0
 		print("Input buffer length frames: ", microphonefeed.get_buffer_length_frames())
-		print("Input buffer length seconds: ", microphonefeed.get_buffer_length_frames()*1.0/input_mix_rate)
+		print("Input buffer length seconds: ", microphone_feed.get_buffer_length_frames() * 1.0 / input_mix_rate)
 	else:
 		microphonefeed.set_active(false)
+
 
 func _on_mic_to_generator_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		$AudioGenerator.stream.mix_rate = input_mix_rate
 	$AudioGenerator.playing = toggled_on
+
 
 func _process(delta: float) -> void:
 	sample_duration += delta
@@ -91,18 +97,19 @@ func _process(delta: float) -> void:
 			audio_sample_image.set_data(audio_sample_size, 1, false, Image.FORMAT_RGF, audio_samples.to_byte_array())
 			audio_sample_texture.update(audio_sample_image)
 			total_samples += 1
-			$SampleCount.text = "%.0f samples/sec" % (total_samples*audio_sample_size/sample_duration)
+			$SampleCount.text = "%.0f samples/sec" % (total_samples * audio_sample_size / sample_duration)
 			if recording_buffer != null:
 				recording_buffer.append(audio_samples)
 			if $MicToGenerator.button_pressed:
 				$AudioGenerator.get_stream_playback().push_buffer(audio_samples)
 	if generator_freq != 0.0:
 		var gplayback: AudioStreamGeneratorPlayback = $AudioGenerator.get_stream_playback()
-		var gdt: float = 1.0/$AudioGenerator.stream.mix_rate
+		var gdt: float = 1.0 / $AudioGenerator.stream.mix_rate
 		for i in range(gplayback.get_frames_available()):
-			var a: float = 0.5*sin(generator_timestamp*generator_freq*TAU)
+			var a: float = 0.5 * sin(generator_timestamp * generator_freq * TAU)
 			gplayback.push_frame(Vector2(a, a))
 			generator_timestamp += gdt
+
 
 func _on_record_button_toggled(toggled_on: bool) -> void:
 	total_samples = 0
@@ -110,7 +117,7 @@ func _on_record_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		$PlayButton.disabled = true
 		$SaveButton.disabled = true
-		recording_buffer = [ ]
+		recording_buffer = []
 		$RecordButton.text = "Stop"
 		$Status.text = "Status: Recording..."
 
@@ -118,7 +125,7 @@ func _on_record_button_toggled(toggled_on: bool) -> void:
 		$PlayButton.disabled = false
 		$SaveButton.disabled = false
 		var recording_data: PackedByteArray = PackedByteArray()
-		var data_size: int = 4*audio_sample_size*len(recording_buffer)
+		var data_size: int = 4 * audio_sample_size * len(recording_buffer)
 		recording_data.resize(44 + data_size)
 		recording_data.encode_u32(0, 0x46464952) # RIFF
 		recording_data.encode_u32(4, len(recording_data) - 8)
@@ -128,26 +135,28 @@ func _on_record_button_toggled(toggled_on: bool) -> void:
 		recording_data.encode_u16(20, 1)
 		recording_data.encode_u16(22, 2)
 		recording_data.encode_u32(24, input_mix_rate)
-		recording_data.encode_u32(28, input_mix_rate*4) # *16*2/8
+		recording_data.encode_u32(28, input_mix_rate * 4) # *16*2/8
 		recording_data.encode_u16(32, 4) # 16*2/8
 		recording_data.encode_u16(34, 16)
 		recording_data.encode_u32(36, 0x61746164) # 'data'
 		recording_data.encode_u32(40, data_size)
 		for i in range(len(recording_buffer)):
 			for j in range(audio_sample_size):
-				var k: int = 44 + 4*(i*audio_sample_size + j)
-				recording_data.encode_s16(k, clampi(recording_buffer[i][j].x*32768, -32768, 32767))
-				recording_data.encode_s16(k+2, clampi(recording_buffer[i][j].y*32768, -32768, 32767))
+				var k: int = 44 + 4 * (i * audio_sample_size + j)
+				recording_data.encode_s16(k, clampi(recording_buffer[i][j].x * 32768, -32768, 32767))
+				recording_data.encode_s16(k + 2, clampi(recording_buffer[i][j].y * 32768, -32768, 32767))
 		wav_recording = AudioStreamWAV.load_from_buffer(recording_data)
 
 		$RecordButton.text = "Record"
 		$Status.text = ""
 		recording_buffer = null
 
+
 func _on_play_button_pressed() -> void:
 	print_rich("\n[b]Playing recording:[/b] %s" % wav_recording)
 	$AudioWav.stream = wav_recording
 	$AudioWav.play()
+
 
 func _on_play_music_toggled(toggled_on: bool) -> void:
 	if toggled_on:
@@ -156,6 +165,7 @@ func _on_play_music_toggled(toggled_on: bool) -> void:
 	else:
 		$AudioMusic.stop()
 		$PlayMusic.text = "Play Music"
+
 
 func _on_save_button_pressed() -> void:
 	var save_path: String = $SaveButton/Filename.text
