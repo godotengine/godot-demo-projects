510e44b4d1bc66c5e8825e84c06912221e135c84
KalmGuy <raphael.gregori.gd@gmail.com>
2025-10-16 12:13:15 +0200

    finish adding comments in main.gd

--- 2d/dodge_the_creeps/main.gd
+++ 2d/dodge_the_creeps/main.gd
@@ -64,25 +64,49 @@ func _on_MobTimer_timeout():
 	mob.position = mob_spawn_location.position
 
 	# Set the mob's direction perpendicular to the path direction.
+	## Notice in the editor that the MobSpawnLocation rotate following the path.
+	## So we can use its rotation to determine the direction of the mob.
+	## We add PI/2 to make the mob move perpendicular to the path.
+	## Remember that in Godot, a rotation is in radians.
+	## Even though we set the rotation in degrees in the editor,
+	## Godot converts it to radians internally.
+	## If you want to use degrees instead of radians,
+	## you can rotation_degrees property instead of rotation.
+	## EXAMPLE : var direction = mob_spawn_location.rotation_degrees + 90
+	## So here we set the direction to point inwards the screen.
 	var direction = mob_spawn_location.rotation + PI / 2
 
 	# Add some randomness to the direction.
+	## randf_range(a, b) returns a random float between a and b.
+	## Here we add a random value between -PI/4 and PI/4 to the direction.
+	## This makes the mob's movement less predictable and more interesting.
 	direction += randf_range(-PI / 4, PI / 4)
+	## finally, set the mob's rotation to the direction.
 	mob.rotation = direction
 
 	# Choose the velocity for the mob.
+	## We create a velocity vector with a random x value between 150 and 250.
+	## and a y value of 0.
 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	## then we rotate the vector of the mob to point in the direction we set.
 	mob.linear_velocity = velocity.rotated(direction)
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
 
-
+## Called when the ScoreTimer times out.
+## Again like a signal function.
+## Check the ScoreTimer node in the editor to see how often it times out.
 func _on_ScoreTimer_timeout():
 	score += 1
+	## Update the HUD with the new score.
+	## We call the update_score() function in hud.gd to update the score display.
 	$HUD.update_score(score)
 
-
+## Called when the StartTimer times out.
+## This timer is a one-shot timer that starts the game after a short delay.
+## We use it to give the player a moment to get ready.
+## It starts the other timers that control the game.
 func _on_StartTimer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
