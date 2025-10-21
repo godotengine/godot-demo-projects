35cf0893066c82cf16ebab3b9190cba8194e3294
KalmGuy <raphael.gregori.gd@gmail.com>
2025-10-21 14:03:54 +0200

    add comments to hud.gd

--- 2d/dodge_the_creeps/hud.gd
+++ 2d/dodge_the_creeps/hud.gd
@@ -16,21 +16,49 @@ func show_game_over():
 	## Show the "Game Over" message for 2 seconds.
 	show_message("Game Over")
 	## Wait for the message timer to time out before showing the start button.
+	## Using await to pause the function until the timer times out.
+	## This requires Godot 4.0 or later.
+	## You can also use a yield() in Godot 3.x, but await is preferred in Godot 4.x.
+	## It sequentally waits for the timer to finish before proceeding to the next line.
+	## ==> Good game flow control technique <==
 	await $MessageTimer.timeout
 	$MessageLabel.text = "Dodge the\nCreeps"
+	## Show the message label again just to be sure it is visible.
 	$MessageLabel.show()
+	## Wait for 1 second before showing the start button.
+	## This gives the player a moment to read the message.
+	## the timer is created on the fly using create_timer().
+	## We don't need to add a Timer node in the scene for this.
+	## again await is used to pause the function until the timer times out.
+	## ==> Another good game flow control technique <==
 	await get_tree().create_timer(1).timeout
+	## Finally, show the start button to allow the player to start a new game.
 	$StartButton.show()
 
-
+## Called in main.gd to update the score display in the HUD.
+## It add +1 each time the timer times out.
 func update_score(score):
+	## Update the ScoreLabel text with the new score.
+	## Convert the score to a string using str() function.
+	## Because the text property expects a string not an integer.
 	$ScoreLabel.text = str(score)
 
-
+## Called when the StartButton is pressed.
+## Signal maked in the editor at the start button node.
 func _on_StartButton_pressed():
 	$StartButton.hide()
+	## Emit the start_game signal to notify the main scene to start the game.
+	## We have to connect this signal in main.gd to a function that starts the game.
+	## We connect it in the main.tscn file using the editor.
+	## In the editor, select the HUD node, go to the Node tab,
+	## find the start_game signal, and connect it to main.gd.
 	start_game.emit()
 
-
+## Called when the MessageTimer times out.
+## The Timer starts when we call show_message().
+## Again like a signal function.
+## you can check the MessageTimer node in the editor to see its wait time.
+## Little trick to hide the message label after showing it for a short time.
+## the signal is embedded in the HUD scene.
 func _on_MessageTimer_timeout():
 	$MessageLabel.hide()
