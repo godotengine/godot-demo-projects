b3ac80e1f5ba3c495b46a07535e49c5e97c867d8
KOGA Mitsuhiro <shiena.jp@gmail.com>
2025-11-28 23:20:37 +0900

    Improve camera demo

--- misc/camera_feed/camerafeed.gd
+++ misc/camera_feed/camerafeed.gd
@@ -1,19 +1,26 @@
 extends Control
 
+const CAMERA_DEACTIVATION_DELAY := 0.1
+const DISPLAY_PADDING := 40.0
+const DEFAULT_WEB_RESOLUTION: Dictionary = {"width": 640, "height": 480}
+
+enum ShaderMode { RGB = 0, YCBCR_SEP = 1, YCBCR = 2 }
+
 @onready var camera_display := $CameraDisplay
-@onready var camera_preview := $CameraDisplay/CameraPreview
+@onready var mirror_container := $CameraDisplay/MirrorContainer
+@onready var rotation_container := $CameraDisplay/MirrorContainer/RotationContainer
+@onready var aspect_container := $CameraDisplay/MirrorContainer/RotationContainer/AspectContainer
+@onready var camera_preview := $CameraDisplay/MirrorContainer/RotationContainer/AspectContainer/CameraPreview
 @onready var camera_list := $DrawerContainer/Drawer/DrawerContent/VBoxContainer/CameraList
 @onready var format_list := $DrawerContainer/Drawer/DrawerContent/VBoxContainer/FormatList
 @onready var start_or_stop_button := $DrawerContainer/Drawer/DrawerContent/VBoxContainer/ButtonContainer/StartOrStopButton
 @onready var reload_button := $DrawerContainer/Drawer/DrawerContent/VBoxContainer/ButtonContainer/ReloadButton
 
 var camera_feed: CameraFeed
-var _initialized: bool = false
-
-const defaultWebResolution: Dictionary = {
-	"width": 640,
-	"height": 480,
-}
+var _initialized := false
+var _cached_formats: Array = []
+var _last_feed_transform: Transform2D
+var _texture_initialized := false
 
 func _ready() -> void:
 	_adjust_ui()
@@ -22,38 +29,63 @@ func _ready() -> void:
 
 
 func _adjust_ui() -> void:
-	camera_display.size = camera_display.get_parent_area_size() - Vector2.ONE * 40
-	camera_preview.custom_minimum_size = camera_display.size
-	camera_preview.position = camera_display.size / 2
+	camera_display.size = camera_display.get_parent_area_size() - Vector2.ONE * DISPLAY_PADDING
+
+	var saved_mirror_scale: Vector2 = mirror_container.scale if mirror_container else Vector2.ONE
+	var saved_rotation: float = rotation_container.rotation if rotation_container else 0.0
+
+	if mirror_container:
+		mirror_container.scale = Vector2.ONE
+		mirror_container.pivot_offset = mirror_container.size / 2
+		mirror_container.scale = saved_mirror_scale
+
+	if rotation_container:
+		rotation_container.rotation = 0.0
+		rotation_container.pivot_offset = rotation_container.size / 2
+		rotation_container.rotation = saved_rotation
+
+	if camera_display.resized.is_connected(_adjust_ui):
+		camera_display.resized.disconnect(_adjust_ui)
+	camera_display.resized.connect(_adjust_ui, ConnectFlags.CONNECT_ONE_SHOT)
 
 
 func _reload_camera_list() -> void:
 	camera_list.clear()
 	format_list.clear()
 
-	var os_name := OS.get_name()
-	# Request camera permission on mobile.
-	if os_name in ["Android", "iOS"]:
-		var permissions = OS.get_granted_permissions()
-		if not "CAMERA" in permissions:
+	# Request camera permission on Android
+	if OS.get_name() == "Android":
+		if not "CAMERA" in OS.get_granted_permissions():
 			if not OS.request_permission("CAMERA"):
 				print("CAMERA permission not granted")
 				return
 
-	if CameraServer.camera_feeds_updated.is_connected(_on_camera_feeds_updated):
-		CameraServer.camera_feeds_updated.disconnect(_on_camera_feeds_updated)
-	CameraServer.camera_feeds_updated.connect(_on_camera_feeds_updated, ConnectFlags.CONNECT_ONE_SHOT)
-
-	if CameraServer.monitoring_feeds:
+	if CameraServer.is_monitoring_feeds:
 		CameraServer.monitoring_feeds = false
 		await get_tree().process_frame
+
+	if not CameraServer.camera_feeds_updated.is_connected(_on_camera_feeds_updated):
+		CameraServer.camera_feeds_updated.connect(_on_camera_feeds_updated, ConnectFlags.CONNECT_DEFERRED)
 
 	CameraServer.monitoring_feeds = true
 
 
 func _on_camera_feeds_updated() -> void:
-	# Get available camera feeds.
-	var feeds = CameraServer.feeds()
+	var feeds := CameraServer.feeds()
+
+	# Skip if feed list hasn't changed
+	if feeds.size() == camera_list.item_count:
+		var all_match := true
+		for i in feeds.size():
+			if feeds[i].get_name() != camera_list.get_item_text(i):
+				all_match = false
+				break
+		if all_match:
+			return
+
+	camera_list.clear()
+	format_list.clear()
+
 	if feeds.is_empty():
 		camera_list.add_item("No cameras found")
 		camera_list.disabled = true
@@ -63,13 +95,10 @@ func _on_camera_feeds_updated() -> void:
 		return
 
 	camera_list.disabled = false
-	for i in range(feeds.size()):
-		var feed: CameraFeed = feeds[i]
+	for feed: CameraFeed in feeds:
 		camera_list.add_item(feed.get_name())
 
-	# Auto-select first camera.
-	camera_list.selected = 0
-	_on_camera_list_item_selected(0)
+	_on_camera_list_item_selected(camera_list.selected)
 
 
 func _on_camera_list_item_selected(index: int) -> void:
@@ -77,15 +106,14 @@ func _on_camera_list_item_selected(index: int) -> void:
 	if index < 0 or index >= camera_feeds.size():
 		return
 
-	# Stop previous camera if active.
+	# Stop previous camera and wait for hardware to fully deactivate
 	if camera_feed and camera_feed.feed_is_active:
 		camera_feed.feed_is_active = false
+		await get_tree().create_timer(CAMERA_DEACTIVATION_DELAY).timeout
 
-	# Get selected camera feed.
 	camera_feed = camera_feeds[index]
-
-	# Update format list.
-	_update_format_list()
+	_cached_formats = []
+	await _update_format_list()
 
 
 func _update_format_list() -> void:
@@ -94,8 +122,8 @@ func _update_format_list() -> void:
 	if not camera_feed:
 		return
 
-	var formats = camera_feed.get_formats()
-	if formats.is_empty():
+	_cached_formats = camera_feed.get_formats()
+	if _cached_formats.is_empty():
 		format_list.add_item("No formats available")
 		format_list.disabled = true
 		start_or_stop_button.disabled = true
@@ -103,9 +131,11 @@ func _update_format_list() -> void:
 
 	format_list.disabled = false
 	start_or_stop_button.disabled = false
-	for format in formats:
-		var resolution := str(format["width"]) + "x" + str(format["height"])
-		var item := "%s - %s" % [format["format"], resolution]
+	for format: Dictionary in _cached_formats:
+		var width: int = format.get("width", 0)
+		var height: int = format.get("height", 0)
+		var format_name: String = format.get("format", "Unknown")
+		var item := "%s - %dx%d" % [format_name, width, height]
 
 		if format.has("frame_denominator") and format.has("frame_numerator"):
 			item += " : %s / %s" % [format["frame_numerator"], format["frame_denominator"]]
@@ -113,9 +143,8 @@ func _update_format_list() -> void:
 			item += " : %s / %s" % [format["framerate_numerator"], format["framerate_denominator"]]
 		format_list.add_item(item)
 
-	# Auto-select first format.
 	format_list.selected = 0
-	_on_format_list_item_selected(0)
+	await _on_format_list_item_selected(0)
 
 
 func _on_format_list_item_selected(index: int) -> void:
@@ -125,10 +154,16 @@ func _on_format_list_item_selected(index: int) -> void:
 	var formats := camera_feed.get_formats()
 	if index < 0 or index >= formats.size():
 		return
+
+	if camera_feed.feed_is_active:
+		camera_feed.feed_is_active = false
+		await get_tree().create_timer(CAMERA_DEACTIVATION_DELAY).timeout
+
 	var os_name := OS.get_name()
-	var parameters: Dictionary = defaultWebResolution if os_name == "Web" else {}
-	camera_feed.feed_is_active = false
+	var parameters: Dictionary = DEFAULT_WEB_RESOLUTION if os_name == "Web" else {}
 	camera_feed.set_format(index, parameters)
+
+	await get_tree().process_frame
 	_start_camera_feed()
 
 
@@ -136,16 +171,75 @@ func _start_camera_feed() -> void:
 	if not camera_feed:
 		return
 
+	_texture_initialized = false
+	_last_feed_transform = Transform2D()
+
 	if not camera_feed.frame_changed.is_connected(_on_frame_changed):
-		camera_feed.frame_changed.connect(_on_frame_changed, ConnectFlags.CONNECT_ONE_SHOT)
-	# Start the feed.
+		camera_feed.frame_changed.connect(_on_frame_changed)
+
 	camera_feed.feed_is_active = true
 
 
-func _on_frame_changed() -> void:
-	var datatype := camera_feed.get_datatype() as CameraFeed.FeedDataType
-	var preview_size := Vector2.ZERO
+func _update_scene_transform() -> void:
+	if not camera_feed or not camera_feed.feed_is_active:
+		return
+	if _cached_formats.is_empty():
+		return
 
+	var mat: ShaderMaterial = camera_preview.material
+	if not mat:
+		return
+
+	var preview_size := _get_preview_size(mat)
+	if preview_size.round() <= Vector2.ZERO:
+		return
+
+	var is_front_camera := camera_feed.get_position() == CameraFeed.FeedPosition.FEED_FRONT
+	mirror_container.scale = Vector2(-1.0 if is_front_camera else 1.0, 1.0)
+
+	rotation_container.rotation = camera_feed.feed_transform.get_rotation()
+
+	var display_size := DisplayServer.window_get_size()
+	if display_size.x > display_size.y:
+		aspect_container.ratio = preview_size.x / preview_size.y
+	else:
+		aspect_container.ratio = preview_size.y / preview_size.x
+
+
+func _get_preview_size(mat: ShaderMaterial) -> Vector2:
+	var datatype := camera_feed.get_datatype() as CameraFeed.FeedDataType
+	match datatype:
+		CameraFeed.FeedDataType.FEED_RGB:
+			var texture: CameraTexture = mat.get_shader_parameter("rgb_texture")
+			if texture:
+				return texture.get_size()
+		CameraFeed.FeedDataType.FEED_YCBCR_SEP:
+			var texture: CameraTexture = mat.get_shader_parameter("y_texture")
+			if texture:
+				return texture.get_size()
+		CameraFeed.FeedDataType.FEED_YCBCR:
+			var texture: CameraTexture = mat.get_shader_parameter("ycbcr_texture")
+			if texture:
+				return texture.get_size()
+	return Vector2.ZERO
+
+
+func _on_frame_changed() -> void:
+	if not camera_feed or not camera_feed.feed_is_active:
+		return
+	if _cached_formats.is_empty():
+		return
+
+	if not _texture_initialized:
+		_setup_textures()
+
+	var current_transform := camera_feed.feed_transform
+	if current_transform != _last_feed_transform:
+		_last_feed_transform = current_transform
+		_update_scene_transform()
+
+
+func _setup_textures() -> void:
 	var mat: ShaderMaterial = camera_preview.material
 	var rgb_texture: CameraTexture = mat.get_shader_parameter("rgb_texture")
 	var y_texture: CameraTexture = mat.get_shader_parameter("y_texture")
@@ -157,50 +251,49 @@ func _on_frame_changed() -> void:
 	cbcr_texture.which_feed = CameraServer.FeedImage.FEED_CBCR_IMAGE
 	ycbcr_texture.which_feed = CameraServer.FEED_YCBCR_IMAGE
 
+	var datatype := camera_feed.get_datatype() as CameraFeed.FeedDataType
+	var preview_size := Vector2.ZERO
+
 	match datatype:
 		CameraFeed.FeedDataType.FEED_RGB:
 			rgb_texture.camera_feed_id = camera_feed.get_id()
 			mat.set_shader_parameter("rgb_texture", rgb_texture)
-			mat.set_shader_parameter("mode", 0)
+			mat.set_shader_parameter("mode", ShaderMode.RGB)
 			preview_size = rgb_texture.get_size()
 		CameraFeed.FeedDataType.FEED_YCBCR_SEP:
 			y_texture.camera_feed_id = camera_feed.get_id()
 			cbcr_texture.camera_feed_id = camera_feed.get_id()
 			mat.set_shader_parameter("y_texture", y_texture)
 			mat.set_shader_parameter("cbcr_texture", cbcr_texture)
-			mat.set_shader_parameter("mode", 1)
+			mat.set_shader_parameter("mode", ShaderMode.YCBCR_SEP)
 			preview_size = y_texture.get_size()
 		CameraFeed.FeedDataType.FEED_YCBCR:
 			ycbcr_texture.camera_feed_id = camera_feed.get_id()
 			mat.set_shader_parameter("ycbcr_texture", ycbcr_texture)
-			mat.set_shader_parameter("mode", 2)
+			mat.set_shader_parameter("mode", ShaderMode.YCBCR)
 			preview_size = ycbcr_texture.get_size()
 		_:
 			print("Skip formats that are not supported.")
 			return
 
+	if preview_size.round() <= Vector2.ZERO:
+		return
+
 	var white_image := Image.create(int(preview_size.x), int(preview_size.y), false, Image.FORMAT_RGBA8)
 	white_image.fill(Color.WHITE)
 	camera_preview.texture = ImageTexture.create_from_image(white_image)
 
-	var rot := camera_feed.feed_transform.get_rotation()
-	var degree := roundi(rad_to_deg(rot))
-	camera_preview.rotation = rot
-	camera_preview.custom_minimum_size.y = camera_display.size.y
-
-	if absi(degree) % 180 == 0:
-		camera_display.ratio = preview_size.x / preview_size.y
-	else:
-		camera_display.ratio = preview_size.y / preview_size.x
-
+	_texture_initialized = true
 	start_or_stop_button.text = "Stop"
 
 
-func _on_start_or_stop_button_pressed(change_label: bool = true) -> void:
+func _on_start_or_stop_button_pressed(change_label := true) -> void:
 	if camera_feed and camera_feed.feed_is_active:
 		camera_feed.feed_is_active = false
+		await get_tree().process_frame
 		camera_preview.texture = null
 		camera_preview.rotation = 0
+		_texture_initialized = false
 		if change_label:
 			start_or_stop_button.text = "Start"
 	else:
@@ -215,8 +308,11 @@ func _on_reload_button_pressed() -> void:
 
 
 func _notification(what: int) -> void:
-	if what == NOTIFICATION_RESIZED and _initialized:
-		_adjust_ui()
+	if not _initialized:
+		return
+	match what:
+		NOTIFICATION_RESIZED, NOTIFICATION_WM_SIZE_CHANGED:
+			_adjust_ui()
 
 
 func _exit_tree() -> void:
