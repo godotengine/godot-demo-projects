e6deff1d0bfc604144f491a456d638a7b6cb1b53
Allen Pestaluky <allenpestaluky@gmail.com>
2025-12-16 14:55:32 -0500

    Reworked colour sweep scene to fit in new window size

--- hdr_output/main/debug_info.gd
+++ hdr_output/main/debug_info.gd
@@ -11,7 +11,7 @@ func _process(_delta: float) -> void:
 		device_has_hdr = device.has_feature(RenderingDevice.SUPPORTS_HDR_OUTPUT)
 	
 	text = ""
-	text += "DisplayServer Supports HDR: %s\n" % DisplayServer.has_feature(DisplayServer.FEATURE_HDR_OUTPUT)
-	text += "RenderingDevice Supports HDR: %s\n" % device_has_hdr
-	text += "Window Supports HDR: %s\n" % DisplayServer.window_is_hdr_output_supported(window.get_window_id())
+	text += "DisplayServer supports HDR: %s\n" % DisplayServer.has_feature(DisplayServer.FEATURE_HDR_OUTPUT)
+	text += "RenderingDevice supports HDR: %s\n" % device_has_hdr
+	text += "Window supports HDR: %s\n" % DisplayServer.window_is_hdr_output_supported(window.get_window_id())
 	
