0897f65d06dc5d52eb8c149238e53ee546681ab1
Allen Pestaluky <allenpestaluky@gmail.com>
2026-01-20 22:10:51 -0500

    Added tooltip text to help debug why hdr output might not be working.

--- hdr_output/main/debug_info.gd
+++ hdr_output/main/debug_info.gd
@@ -5,13 +5,19 @@ func _process(_delta: float) -> void:
 	
 	var device := RenderingServer.get_rendering_device();
 	
-	var device_has_hdr: bool
-	
+	var device_has_hdr: bool = false
 	if device:		
 		device_has_hdr = device.has_feature(RenderingDevice.SUPPORTS_HDR_OUTPUT)
 	
 	text = ""
-	text += "DisplayServer supports HDR: %s\n" % DisplayServer.has_feature(DisplayServer.FEATURE_HDR_OUTPUT)
-	text += "RenderingDevice supports HDR: %s\n" % device_has_hdr
+	text += "Display Server supports HDR: %s\n" % DisplayServer.has_feature(DisplayServer.FEATURE_HDR_OUTPUT)
+	text += "Rendering Device supports HDR: %s\n" % device_has_hdr
 	text += "Window supports HDR: %s\n" % DisplayServer.window_is_hdr_output_supported(window.get_window_id())
+	
+	if !DisplayServer.has_feature(DisplayServer.FEATURE_HDR_OUTPUT):
+		tooltip_text = "Display Server does not support\nHDR output. This usually means\nthat Godot does not support HDR\noutput on your current platform,\nbut other Display Server drivers may support HDR output.\n\n"
+	elif !device_has_hdr:
+		tooltip_text = "Rendering Device does not support\nHDR output. Try changing the\nRendering Device driver in your\nproject settings to a driver that supports\nHDR output, such as d3d12 for Windows\nand metal for Mac OS.\n\n"
+	elif !DisplayServer.window_is_hdr_output_supported(window.get_window_id()):
+		tooltip_text = "Window does not support HDR output.\nPlease ensure that your window is\npositioned on a screen that is currently\nin HDR mode."
 	
