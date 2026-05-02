e0faf39a3aaf34b108498c5544680f1fa44059bf
Hoodedgull <victor.s@live.dk>
2026-05-02 19:31:30 +0200

    replace bomb

---
+++ networking/multiplayer_bomber_cs/Bomb.cs
@@ -0,0 +1,61 @@
+using Godot;
+using System;
+using System.Linq;
+using Array = Godot.Collections.Array;
+
+public partial class Bomb : Area2D
+{
+	private Array in_area = new ();
+	private int from_player;
+	
+	// Called from the animation.
+	public void explode()
+	{
+		if (!IsMultiplayerAuthority())
+		{
+			// Explode only on authority.
+			return;
+		}
+
+		for (int i = 0; i < in_area.Count; i++)
+		{
+			var item = in_area[i].AsGodotObject();
+			if (item.HasMethod("exploded"))
+			{
+			   // Checks if there is wall in between bomb and the object.
+			   var worldState = GetWorld2D().DirectSpaceState;
+			   var query = PhysicsRayQueryParameters2D.Create(Position, item.Get("position").AsVector2());
+			   query.HitFromInside = true;
+			   var result = worldState.IntersectRay(query);
+			   ;
+			   // TODO: double check. GDScript: `if result.collider is not TileMap:`
+			   if(result.TryGetValue("collider", out var collider) && collider.AsGodotObject() is not TileMapLayer)
+			   {
+				   item.Call("exploded", from_player);
+			   }
+			}
+		}
+	}
+
+	void done()
+	{
+		if (IsMultiplayerAuthority())
+		{
+			QueueFree();
+		}
+	}
+
+	void _on_bomb_body_enter(Node2D body)
+	{
+		if (!in_area.Contains(body))
+		{
+			in_area.Add(body);
+		}
+	}
+
+	void _on_bomb_body_exit(Node2D body)
+	{
+		in_area.Remove(body);
+	}
+	
+}
--- networking/multiplayer_bomber_cs/Score.cs
+++ networking/multiplayer_bomber_cs/Score.cs
@@ -4,71 +4,71 @@ using System.Collections.Generic;
 
 public partial class Score : HBoxContainer
 {
-    private class PlayerLabel
-    {
-        public int Score;
-        public string Name;
-        public Label Label;
-    }
-    private Dictionary<int, PlayerLabel> playerLabels = new();
-    
-    public override void _Ready()
-    {
-        GetNode<Label>("../Winner").Hide();
-    }
+	private class PlayerLabel
+	{
+		public int Score;
+		public string Name;
+		public Label Label;
+	}
+	private Dictionary<int, PlayerLabel> playerLabels = new();
+	
+	public override void _Ready()
+	{
+		GetNode<Label>("../Winner").Hide();
+	}
 
-    public override void _Process(double delta)
-    {
-        var rocksLeft = GetNode("../Rocks").GetChildCount();
-        if (rocksLeft == 0)
-        {
-            var winnerName = "";
-            var winnerScore = 0;
-            foreach (var player in playerLabels.Values)
-            {
-                if (player.Score > winnerScore)
-                {
-                    winnerScore = player.Score;
-                    winnerName = player.Name;
-                }
-            }
-            
-            GetNode<Label>("../Winner").SetText("THE WINNER IS:\n" + winnerName);
-            GetNode<Label>("../Winner").Show();
-        }
-    }
+	public override void _Process(double delta)
+	{
+		var rocksLeft = GetNode("../Rocks").GetChildCount();
+		if (rocksLeft == 0)
+		{
+			var winnerName = "";
+			var winnerScore = 0;
+			foreach (var player in playerLabels.Values)
+			{
+				if (player.Score > winnerScore)
+				{
+					winnerScore = player.Score;
+					winnerName = player.Name;
+				}
+			}
+			
+			GetNode<Label>("../Winner").SetText("THE WINNER IS:\n" + winnerName);
+			GetNode<Label>("../Winner").Show();
+		}
+	}
 
-    void increase_score(int for_who)
-    {
-        var player = playerLabels[for_who];
-        player.Score += 1;
-        player.Label.SetText(player.Name + "\n" + player.Score);
-    }
+	void increase_score(int for_who)
+	{
+		var player = playerLabels[for_who];
+		player.Score += 1;
+		player.Label.SetText(player.Name + "\n" + player.Score);
+	}
 
-    void add_player(int id, string new_player_name)
-    {
-        var label = new Label();
-        label.HorizontalAlignment = HorizontalAlignment.Center;
-        label.Text = new_player_name + "\n" + 0;
-        label.Modulate = GetNode("/root/gamestate").Call("get_player_color", new_player_name).AsColor();
-        label.SizeFlagsHorizontal = SizeFlags.ExpandFill;
-        label.AddThemeFontOverride("font", GD.Load<Font>("res://montserrat.otf"));
-        label.AddThemeColorOverride("font_outline_color", Colors.Black);
-        label.AddThemeConstantOverride("outline_size", 9);
-        label.AddThemeFontSizeOverride("font_size", 18);
-        AddChild(label);
-        
-        playerLabels[id] = new PlayerLabel()
-        {
-            Name = new_player_name,
-            Label = label,
-            Score = 0
-        };
-        
-    }
+	void add_player(int id, string new_player_name)
+	{
+		var label = new Label();
+		label.HorizontalAlignment = HorizontalAlignment.Center;
+		label.Text = new_player_name + "\n" + 0;
+		label.Modulate = GetNode("/root/gamestate").Call("get_player_color", new_player_name).AsColor();
+		label.SizeFlagsHorizontal = SizeFlags.ExpandFill;
+		label.AddThemeFontOverride("font", GD.Load<Font>("res://montserrat.otf"));
+		label.AddThemeColorOverride("font_outline_color", Colors.Black);
+		label.AddThemeConstantOverride("outline_size", 9);
+		label.AddThemeFontSizeOverride("font_size", 18);
+		AddChild(label);
+		
+		playerLabels[id] = new PlayerLabel()
+		{
+			Name = new_player_name,
+			Label = label,
+			Score = 0
+		};
+		
+	}
 
-    public void _on_exit_game_pressed()
-    {
-        GetNode("/root/gamestate").Call("end_game");
-    }
+	public void _on_exit_game_pressed()
+	{
+		GetNode("/root/gamestate").Call("end_game");
+	}
 }
