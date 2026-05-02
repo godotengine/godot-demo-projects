0a533768af24ceca9f74b60c8220b995e93f5753
Hoodedgull <victor.s@live.dk>
2026-05-03 00:07:34 +0200

    more fixes

--- networking/multiplayer_bomber/player_spawner.gd
+++ networking/multiplayer_bomber/player_spawner.gd
@@ -4,11 +4,12 @@ func _init() -> void:
 	spawn_function = _spawn_player
 
 
-func _spawn_player(data: Array) -> Area2D:
-	if data.size() != 2 or typeof(data[0]) != TYPE_VECTOR2 or typeof(data[1]) != TYPE_INT:
+func _spawn_player(data: Array) -> CharacterBody2D:
+	if data.size() != 3 or typeof(data[0]) != TYPE_VECTOR2 or typeof(data[1]) != TYPE_INT or typeof(data[2]) != TYPE_STRING:
 		return null
 
-	var player: Area2D = preload("res://player.tscn").instantiate()
-	player.position = data[0]
-	player.name = data[1]
+	var player: CharacterBody2D = preload("res://player.tscn").instantiate()
+	player.synced_position = data[0]
+	player.name = str(data[1])
+	player.set_player_name(data[2])
 	return player
