5de55ab0e49b8ebc4b054da740869cef04df2023
Rob Murray <rmfiremint@gmail.com>
2026-05-25 13:44:22 +1000

    Improve 2d/physics_tests for debugging and review
    Add command line launch options for direct debugging
    2 new demos to aid in solver debugging and review
    Add ‘pause stepping’ by pressing ‘L’
    Add Solver Iteration settings and bias controls
    Small fixes and improvements
    Lightly refactor UI and code as necessary
    
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>

--- 2d/physics_tests/utils/time_scale.gd
+++
@@ -1,8 +0,0 @@
-extends HBoxContainer
-
-
-func _on_h_slider_value_changed(value: float) -> void:
-	value = maxf(0.1, value)
-	$Value.text = "%.1f×" % value
-	Engine.time_scale = value
-	Engine.physics_ticks_per_second = $"../TicksPerSecond/HSlider".value * value
