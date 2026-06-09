ebdb2ef202c09183c5e65ff35969eede36bd42e8
Hugo Locurcio <hugo.locurcio@hugo.pro>
2026-06-09 17:42:08 +0200

    Improve Control Gallery demo (#1315)
    - Add CodeEdit, GraphEdit, GraphFrame, and GraphNode examples.
    - Add icons (enabled/disabled) to TreeItem example.
    - Add placeholder texts to LineEdit, TextEdit, and CodeEdit.
    - Allow tab navigation in TextEdit (pressing Tab moves focus).
    - Fix sizing of "Basic controls" column so that it completely fits
      at the default window size.

--- gui/control_gallery/tree.gd
+++ gui/control_gallery/tree.gd
@@ -4,10 +4,15 @@ extends Tree
 
 func _ready() -> void:
 	var root: TreeItem = create_item()
-	root.set_text(0, "Tree - Root")
+	root.set_text(0, "Tree - Root TreeItem")
+	var image := preload("res://icon.webp").get_image()
+	image.resize(16, 16)
+	root.add_button(0, ImageTexture.create_from_image(image), -1, false, "Example TreeItem button.")
+	root.add_button(0, ImageTexture.create_from_image(image), -1, true, "Example disabled TreeItem button.")
+
 	var child1: TreeItem = create_item(root)
-	child1.set_text(0, "Tree - Child 1")
+	child1.set_text(0, "Tree - TreeItem 1")
 	var child2: TreeItem = create_item(root)
-	child2.set_text(0, "Tree - Child 2")
+	child2.set_text(0, "Tree - TreeItem 2")
 	var subchild1: TreeItem = create_item(child1)
-	subchild1.set_text(0, "Tree - Subchild 1")
+	subchild1.set_text(0, "Tree - TreeItem 1 Child")
