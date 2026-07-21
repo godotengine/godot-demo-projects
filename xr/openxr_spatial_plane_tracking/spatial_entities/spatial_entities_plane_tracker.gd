8afbc94aae28b24453fbf03c9679938a5bb93ba0
Bastiaan Olij <mux213@gmail.com>
2026-07-21 18:19:45 +1000

    Reconstruct mesh from vertex data to fix wireframe representation

--- xr/openxr_spatial_plane_tracking/spatial_entities/spatial_entities_plane_tracker.gd
+++ xr/openxr_spatial_plane_tracking/spatial_entities/spatial_entities_plane_tracker.gd
@@ -20,10 +20,34 @@ func _update_mesh_and_collision():
 		# and mesh are positioned correctly.
 		$StaticBody3D.transform = _plane_tracker.get_mesh_offset()
 
-		# Set our mesh so we can occlude the surface (see material override)
-		var new_mesh = _plane_tracker.get_mesh()
-		if $StaticBody3D/MeshInstance3D.mesh != new_mesh:
-			$StaticBody3D/MeshInstance3D.mesh = new_mesh
+		# Set our mesh so we can occlude the surface (see material override).
+		if _plane_tracker.has_method("get_vertices"):
+			# Available in 4.8 onwards, get our mesh data
+			var org_vertices = _plane_tracker.get_vertices()
+			var org_indices = _plane_tracker.get_indices()
+
+			if not org_vertices.is_empty() and not org_indices.is_empty():
+				# We need to make sure very triangle has unique vertices
+				# for our wireframe shader.
+				var vertices := PackedVector3Array()
+				vertices.resize(org_indices.size())
+				for i in range(org_indices.size()):
+					var v = org_vertices[org_indices[i]]
+					vertices[i] = Vector3(v.x, v.y, 0.0)
+
+				var mesh_array := Array()
+				mesh_array.resize(Mesh.ARRAY_MAX)
+				mesh_array[Mesh.ARRAY_VERTEX] = vertices
+
+				var new_mesh := ArrayMesh.new()
+				new_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mesh_array)
+				$StaticBody3D/MeshInstance3D.mesh = new_mesh
+		else:
+			# `get_mesh` is cached and the better option if you're just interested
+			# in the geometry. But it will fail with our wireframe shader.
+			var new_mesh = _plane_tracker.get_mesh()
+			if $StaticBody3D/MeshInstance3D.mesh != new_mesh:
+				$StaticBody3D/MeshInstance3D.mesh = new_mesh
 
 		# And set our shape so we can have things collide with our surface.
 		var new_shape = _plane_tracker.get_shape()
